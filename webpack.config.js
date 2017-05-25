f87d2044e401e8afb60ce57e4b497af5779013bf
Todd <todd@websbytodd.com>
2017-05-25 16:57:08 -0400

    TypeScript, React, & Webpack config files.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  entry: __dirname + "/src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/docs"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { 
+        test: /\.tsx?$/,
+        exclude: /node_modules/, 
+        loader: "awesome-typescript-loader" 
+      },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { 
+        enforce: "pre", 
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "source-map-loader" 
+      }
+    ]
+  },
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
+};
