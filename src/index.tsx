3640159a10ce0fed88c7cb367169542df661ffc4
Todd <todd@websbytodd.com>
2017-05-26 18:07:35 -0400

    Root element is TakeEditor

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import TakeEditor from "./components/TakeEditor";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <TakeEditor />,
     document.getElementById("app")
 );
