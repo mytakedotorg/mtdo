4e32f27e11d61c619372564a05fd1ea59db8c49c
Todd <todd@websbytodd.com>
2017-05-26 20:28:02 -0400

    Don't generate source-map file. Too large and don't need it yet.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  //devtool: "source-map",
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
