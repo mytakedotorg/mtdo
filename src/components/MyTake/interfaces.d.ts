2a665df191ae21c04931c2a285a9f7501329905c
Todd <todd@websbytodd.com>
2017-06-22 19:54:25 -0400

    Defined MyTakeState interface.

--- src/components/MyTake/interfaces.d.ts
+++ src/components/MyTake/interfaces.d.ts
@@ -3,5 +3,9 @@ interface MyTakeProps {
 }
 
 interface MyTakeState {
-  
+  constitutionNodes: Array<MyReactComponentObject>,
+  textIsHighlighted: boolean,
+  dragData: string,
+  editorState: any,         //Defined by slate. No it's not. You got this.
+  schema: any,              //Defined by slate. No it's not. You got this.
 }
