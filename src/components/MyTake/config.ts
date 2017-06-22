761354d8a8291b58147aa819fc134973fc0eb355
Todd <todd@websbytodd.com>
2017-06-22 19:53:03 -0400

    Initial commit.

---
+++ src/components/MyTake/config.ts
@@ -0,0 +1,27 @@
+export default {
+  initialState: {
+    nodes: [
+      {
+        kind: 'block',
+        type: 'title',
+        nodes: [
+          {
+            kind: 'text',
+            text: ''
+          }
+        ]
+      },
+      {
+        kind: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            kind: 'text',
+            text: '',
+           
+          }
+        ]
+      }
+    ]
+  }
+}
