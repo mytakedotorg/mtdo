ffffe6efcde7102f570a04e3d8c98ef80b681db3
Todd <todd@websbytodd.com>
2017-06-30 17:41:31 -0400

    Moved src/stylesheets/ to src/assets/stylesheets/

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import MyTake from "./components/MyTake";
 
-require('./stylesheets/main.scss');
+require('./assets/stylesheets/main.scss');
 
 ReactDOM.render(
     <MyTake />,
