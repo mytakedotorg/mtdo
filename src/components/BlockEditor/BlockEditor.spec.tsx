a04a3ecfb02d641913d8f96dfcac8799a3b4de26
Ned Twigg <ned.twigg@diffplug.com>
2017-07-17 02:22:08 -0700

    First cut at a simple BlockEditor.

---
+++ src/components/BlockEditor/BlockEditor.spec.tsx
@@ -0,0 +1,50 @@
+// https://facebook.github.io/jest/docs/snapshot-testing.html#content
+
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+import { mount } from 'enzyme';
+import BlockEditor from './BlockEditor';
+import { TakeDocument } from './BlockEditor';
+
+let doc: TakeDocument = {
+    title: "My take title",
+    blocks: [
+        {
+            kind: 'paragraph',
+            text: 'Some text'
+        },
+        {
+            kind: 'document',
+            document: 'Constitution',
+            range: [1, 25]
+        }
+    ]
+};
+
+test('Simple block editor model', () => {
+    const tree = renderer.create(
+        <BlockEditor blocks={doc.blocks} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+test('With active', () => {
+    const tree = renderer.create(
+        <BlockEditor blocks={doc.blocks} active={0} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+test('With hover', () => {
+    const tree = renderer.create(
+        <BlockEditor blocks={doc.blocks} hover={0} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+test('With hover and active', () => {
+    const tree = renderer.create(
+        <BlockEditor blocks={doc.blocks} hover={0} active={0} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+})
---
+++ src/components/BlockEditor/BlockEditor.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+
+////////////////////
+// Document model //
+////////////////////
+export interface ParagraphBlock {
+    kind: 'paragraph';
+    text: string;
+}
+export interface DocumentBlock {
+    kind: 'document';
+    document: string;
+    range: [number, number];
+}
+export type TakeBlock = ParagraphBlock | DocumentBlock;
+
+export interface TakeDocument {
+    title: string;
+    blocks: TakeBlock[];
+}
+
+/////////////////
+// React model //
+/////////////////
+function Paragraph(props: ParagraphBlock) {
+    return (<div className="paragraph">{props.text}</div>)
+}
+
+function Document(props: DocumentBlock) {
+    return (<div className="document">{props.document} range {props.range.toString()}</div>)
+}
+
+export interface BlockContainerProps {
+    block: TakeBlock;
+    active: boolean;
+    hover: boolean;
+}
+
+function BlockContainer(props: BlockContainerProps) {
+    let inner;
+    switch (props.block.kind) {
+        case 'paragraph': inner = Paragraph(props.block); break;
+        case 'document':  inner = Document(props.block);  break;
+    }
+
+    let classes = 'blockcontainer';
+    if (props.hover) {
+        classes += ' blockcontainer__hover'
+    }
+    if (props.active) {
+        classes += ' blockcontainer__active'
+    }
+    return (
+        <div className={classes}>
+            {inner}
+        </div>
+    )
+}
+
+export interface BlockEditorProps {
+    blocks: TakeBlock[];
+    active?: number;
+    hover?: number;
+}
+
+export function BlockEditor(props: BlockEditorProps) {
+    const blocks = props.blocks.map((block, idx) =>
+            <BlockContainer key={idx.toString()}
+                block={block}
+                active={idx === props.active}
+                hover={idx === props.hover} />
+        )
+    return (<div className="blockeditor">{blocks}</div>)
+}
+
+export default BlockEditor;
