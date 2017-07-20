2411c08b9e350c17da8e2f5536323515505b7aba
Todd <todd@websbytodd.com>
2017-07-20 08:51:01 -0400

    Uninstalled Slate.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-const { Raw } = require('slate');
 import MyTake from "./components/MyTake";
 import config from "./components/MyTake/config";
 
 let app: HTMLElement = document.getElementById("app");
-let initJson;
-if (app.hasAttribute('data-init')) {
-    initJson = JSON.parse(app.getAttribute('data-init'));
-} else {
-    initJson = config.initialState;
-}
-let initStateParsed = Raw.deserialize(initJson, { terse: true });
+// let initJson;
+// if (app.hasAttribute('data-init')) {
+//     initJson = JSON.parse(app.getAttribute('data-init'));
+// } else {
+//     initJson = config.initialState;
+// }
+// let initStateParsed = Raw.deserialize(initJson, { terse: true });
 
-ReactDOM.render(<MyTake initState={initStateParsed}/>, app)
+const Root = (
+	<MyTake />
+);
+
+ReactDOM.render(Root, app)
