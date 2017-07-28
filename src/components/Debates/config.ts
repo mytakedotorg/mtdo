e2e098db3435da3f76eb25058cdcdf085ed2ab2f
Todd <todd@websbytodd.com>
2017-07-28 19:10:30 -0400

    Initial commit. To be used as initial state for Debates component.

---
+++ src/components/Debates/config.ts
@@ -0,0 +1,19 @@
+export default {
+  videos: [
+    {
+      id: "qAqIKybNO38",
+      title: "Jimmy Carter - Gerald Ford #1",
+      date: "Sep 23, 1976"
+    },
+    {
+      id: "TjHjU0Eu26Y",
+      title: "Jimmy Carter - Gerald Ford #2",
+      date: "Oct 6, 1976"
+    },
+    {
+      id: "CipT04S0bVE",
+      title: "Jimmy Carter - Gerald Ford #3",
+      date: "Oct 22, 1976"
+    }
+  ]
+};
