7a9cba210161cfb6f4334174863daeaf64ce1c0b
Ned Twigg <ned.twigg@diffplug.com>
2017-08-12 01:13:49 -0700

    Reorganized gulp to have a dev and a prod build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,106 @@
 const gulp = require('gulp')
+  // sass
+  sass = require('gulp-sass')
+  autoprefixer = require('gulp-autoprefixer')
+  concat = require('gulp-concat')
+  notify = require('gulp-notify')
+  // webpack
   webpackCore = require('webpack')
   webpack = require('webpack-stream')
   path = require('path')
   fs = require('fs')
-  gutil = require('gulp-util')
+  // html
   nunjucks = require('gulp-nunjucks-html')
-  concat = require('gulp-concat')
-  autoprefixer = require('gulp-autoprefixer')
-  sass = require('gulp-sass')
-  notify = require('gulp-notify')
+  // misc
   browserSync = require('browser-sync').create();
+  rev = require('gulp-rev');
 
 const config = {
-  siteRoot: './dist',
-  publicDir: './dist/public',
+  dist: './dist',
+  sassSrc:  './assets/stylesheets/**/*.?(s)css',
+  webpackSrc: ['./src/**/*', '!src/**/*.spec.js'],
   nunjucksTemplates: './nunjucks/templates',
   nunjucksPages: './nunjucks/pages',
-  siteSrc: ['./src/**/*', '!src/**/*.spec.js'],
-  sassFilter:  './assets/stylesheets/**/*.?(s)css'
 }
 
-gulp.task('css', () => {
-	gulp.src(config.sassFilter)
-		.pipe(sass({
-			style: 'compressed'
-			}).on("error", notify.onError(function (error) {
-				return "Error: " + error.message;
-			})))
-		.pipe(autoprefixer({cascade: false, browsers: ['> 0.25%']}))
-		.pipe(concat('all.css'))
-		.pipe(gulp.dest(config.publicDir))
-});
+///////////////////////////////
+// Create dev and prod tasks //
+///////////////////////////////
+const BUILD = 'build'
+const SERVE = 'serve'
 
-gulp.task('webpack', (cb) => {
-  gulp.src(config.siteSrc)
-    .pipe(webpack({
-      config : require('./webpack.config.js')
-    }, webpackCore, (err, stats) => {
-      // workaround for https://github.com/shama/webpack-stream/issues/161
-      const abspath = path.resolve(config.siteRoot + '/manifest.json')
-      const content = stats.compilation.compiler.outputFileSystem.readFileSync(abspath)
-      fs.writeFileSync(abspath, content)
-      // makes this task blocking
-      cb(err)
-    }))
-    .pipe(gulp.dest(config.siteRoot))
-});
+function setup(mode) {
+  const sass = 'sass' + mode;
+  const webpack = 'webpack' + mode;
+  const nunjucks = 'nunjucks' + mode;
+  gulp.task(sass, sassCfg(mode))
+  gulp.task(webpack, webpackCfg(mode))
+  gulp.task(nunjucks, [webpack], nunjucksCfg(mode))
 
-gulp.task('nunjucks', ['webpack'], () => {
-  gulp.src(config.nunjucksPages + '/**/*.html')
-    .pipe(nunjucks({
-      searchPaths: [config.nunjucksTemplates],
-      locals: { manifest: require(config.siteRoot + '/manifest.json') }
-    }))
-    .pipe(gulp.dest(config.siteRoot))
-});
+  gulp.task(BUILD + mode, [nunjucks])
+  gulp.task(SERVE + mode, browserSyncCfg(mode))
+}
+const DEV = 'Dev'
+const PROD = 'Prod'
+setup(DEV)
+setup(PROD)
 
-gulp.task('serve', () => {
-  browserSync.init({
-    files: [config.siteRoot + '/**'],
-    port: 4000,
-    server: {
-      baseDir: config.siteRoot
-    }
-  });
-  
-  gulp.watch([config.nunjucksTemplates + '/**/*.html', 
-              config.nunjucksPages + '/**/*.html'], ['nunjucks'])
-  gulp.watch(config.siteSrc, ['webpack']);
-  gulp.watch(config.sassFilter, ['css']);
-});
+gulp.task(SERVE, [SERVE + DEV])
+gulp.task(BUILD, [BUILD + PROD])
+gulp.task('default', [SERVE]);
 
-gulp.task('default', ['css', 'webpack', 'nunjucks', 'serve']);
-gulp.task('build', ['css', 'webpack', 'nunjucks']);
+//////////////////////
+// Config functions //
+//////////////////////
+function browserSyncCfg(mode) {
+  return () => {
+    browserSync.init({
+      files: [config.dist + '/**'],
+      server: {
+        baseDir: config.dist
+      }
+    });
+    gulp.watch([config.nunjucksTemplates + '/**/*.html', 
+                config.nunjucksPages + '/**/*.html'], ['nunjucks' + mode]);
+    gulp.watch(config.webpackSrc, ['webpack' + mode]);
+    gulp.watch(config.sassSrc, ['css' + mode]);
+  }
+}
+
+function sassCfg(mode) {
+  return () => {
+    gulp.src(config.sassSrc)
+      .pipe(sass({
+        style: 'compressed'
+        }).on("error", notify.onError(function (error) {
+          return "Error: " + error.message;
+        })))
+      .pipe(autoprefixer({cascade: false, browsers: ['> 0.25%']}))
+      .pipe(concat('all.css'))
+      .pipe(gulp.dest(config.dist))
+  }
+}
+
+function nunjucksCfg(mode) {
+  return () => {
+    gulp.src(config.nunjucksPages + '/**/*.html')
+      .pipe(nunjucks({
+        searchPaths: [config.nunjucksTemplates],
+        locals: { manifest: require(config.dist + '/manifest.json') }
+      }))
+      .pipe(gulp.dest(config.dist))
+  }
+}
+
+function webpackCfg(mode) {
+  return (cb) => {
+    gulp.src(config.webpackSrc)
+      .pipe(webpack({
+        config : require('./webpack.config.js')
+      }, webpackCore, (err) => {
+        // makes this task blocking
+        cb(err)
+      }))
+      .pipe(gulp.dest(config.dist))
+  }
+}
