ae8916f9991f2f314f23324cc19b402b1baca74d
Ned Twigg <ned.twigg@diffplug.com>
2017-08-14 00:08:09 -0700

    Welp, turns out that netlify does asset fingerprinting and minification for us.  DAMMIT.  Better off letting them handle it, I think.  The "dev/prod" stuff is now probably too heavy.  I'll keep it for now...

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,11 @@ const gulp = require('gulp')
   nunjucks = require('gulp-nunjucks-html')
   // misc
   browserSync = require('browser-sync').create();
-  rev = require('gulp-rev');
   tasklisting = require('gulp-task-listing');
   del = require('del')
-  flatmap = require('gulp-flatmap')
 
 const config = {
   dist: './dist',
-  unhashed: './dist-unhashed',
   sassSrc:  './assets/stylesheets/**/*.?(s)css',
   webpackSrc: ['./src/**/*', '!src/**/*.spec.js'],
   nunjucksTemplates: './nunjucks/templates',
@@ -45,30 +42,14 @@ function setupPipeline(mode) {
   const nunjucks = 'nunjucks' + mode;
   gulp.task(sass, sassCfg(mode))
   gulp.task(webpack, webpackCfg(mode))
-
-  if (mode === DEV) {
-    gulp.task(nunjucks, [webpack], nunjucksCfg(mode))
-  } else {
-    const revtask = 'rev' + mode;
-    gulp.task(revtask, [sass, webpack], () => {
-      gulp.src([config.unhashed + '/*.css', config.unhashed + '/*.js'])
-        .pipe(rev())
-        .pipe(gulp.dest(config.dist))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest(config.unhashed))
-      gulp.src([config.unhashed + '/*.jpg', config.unhashed + '/*.js'])
-        .pipe(gulp.dest(config.dist))
-    })
-    gulp.task(nunjucks, [revtask], nunjucksCfg(mode))
-  }
-
-  gulp.task(BUILD + mode, [nunjucks])
-  gulp.task(SERVE + mode, [nunjucks], browserSyncCfg(mode))
+  gulp.task(nunjucks, [webpack], nunjucksCfg(mode))
+  gulp.task(BUILD + mode, [nunjucks, sass])
+  gulp.task(SERVE + mode, [BUILD + mode], browserSyncCfg(mode))
 }
 
 gulp.task('default', tasklisting.withFilters(/clean|default|sass|webpack|nunjucks|rev|default/))
 gulp.task('clean', () => {
-  return del([config.dist, config.unhashed])
+  return del([config.dist])
 })
 
 //////////////////////
@@ -84,7 +65,7 @@ function sassCfg(mode) {
         })))
       .pipe(autoprefixer({cascade: false, browsers: ['> 0.25%']}))
       .pipe(concat('all.css'))
-      .pipe(gulp.dest(config.unhashed))
+      .pipe(gulp.dest(config.dist))
   }
 }
 
@@ -97,49 +78,31 @@ function webpackCfg(mode) {
         // makes this task blocking
         cb(err)
       }))
-      .pipe(gulp.dest(config.unhashed))
+      .pipe(gulp.dest(config.dist))
   }
 }
 
 function nunjucksCfg(mode) {
-  function forManifest(manifest) {
+  return () => {
     return gulp.src(config.nunjucksPages + '/**/*.html')
       .pipe(nunjucks({
         searchPaths: [config.nunjucksTemplates],
-        locals: {
-          manifest: manifest
-        }
       }))
-  }
-  return () => {
-    if (mode == DEV) {
-      var passthrough = {}
-      for (key in ['all.css', 'app.bundle.js', 'blockEditor.bundle.js']) {
-        passthrough[key] = key
-      }
-      return forManifest(passthrough).pipe(gulp.dest(config.unhashed))
-    } else {
-      gulp.src(config.unhashed + '/rev-manifest.json')
-        .pipe(flatmap((stream, file) => {
-          var contents = JSON.parse(file.contents.toString('utf8'));
-          return forManifest(contents).pipe(gulp.dest(config.dist))
-        }))
-    }
+      .pipe(gulp.dest(config.dist))
   }
 }
 
 function browserSyncCfg(mode) {
   return () => {
-    dir = mode === PROD ? config.dist : config.unhashed
     browserSync.init({
-      files: [dir + '/**'],
+      files: [config.dist + '/**'],
       server: {
-        baseDir: dir
+        baseDir: config.dist
       }
     });
     gulp.watch([config.nunjucksTemplates + '/**/*.html', 
                 config.nunjucksPages + '/**/*.html'], ['nunjucks' + mode]);
     gulp.watch(config.webpackSrc, ['webpack' + mode]);
-    gulp.watch(config.sassSrc, ['css' + mode]);
+    gulp.watch(config.sassSrc, ['sass' + mode]);
   }
 }
