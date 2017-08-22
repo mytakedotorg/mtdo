316cf497e67ca90661a161e5b2ff9235d43c89f2
Todd <todd@websbytodd.com>
2017-08-22 15:23:48 -0400

    Ran "Prettier", shoudl have done this in previous commit.

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,37 +1,37 @@
 import { FoundationTextType } from "../components/Foundation";
 
 export interface BlockIndexMetaData {
-	type: FoundationTextType;
-	range: [number, number];
+  type: FoundationTextType;
+  range: [number, number];
 }
 interface BlockIndex {
-	[index: number]: BlockIndexMetaData;
+  [index: number]: BlockIndexMetaData;
 }
 interface ArticleMetaData {
-	[title: string]: BlockIndex;
+  [title: string]: BlockIndex;
 }
 interface UserMetaData {
-	[user: string]: ArticleMetaData;
+  [user: string]: ArticleMetaData;
 }
 
 const database: UserMetaData = {
-	"samples": {
+  samples: {
     "does-a-law-mean-what-it-says-or-what-it-meant": {
-			6: {
-				type: 'AMENDMENTS',
-				range: [369, 514]
-			},
-			9: {
-				type: 'AMENDMENTS',
-				range: [369, 442]
-			}
-		},
-		"dont-worry-well-protect-the-constitution-for-you": {
-			1: {
-				type: 'CONSTITUTION',
-				range: [19343, 19970]
-			}
-		}
+      6: {
+        type: "AMENDMENTS",
+        range: [369, 514]
+      },
+      9: {
+        type: "AMENDMENTS",
+        range: [369, 442]
+      }
+    },
+    "dont-worry-well-protect-the-constitution-for-you": {
+      1: {
+        type: "CONSTITUTION",
+        range: [19343, 19970]
+      }
+    }
   }
 };
 
