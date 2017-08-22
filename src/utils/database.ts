732c7b00e1accdedd9eb8b620c84873db1cbcab5
Todd <todd@websbytodd.com>
2017-08-22 15:22:02 -0400

    Will use this as our client side "database" that stores user and article metadata. It is read only.

---
+++ src/utils/database.ts
@@ -0,0 +1,38 @@
+import { FoundationTextType } from "../components/Foundation";
+
+export interface BlockIndexMetaData {
+	type: FoundationTextType;
+	range: [number, number];
+}
+interface BlockIndex {
+	[index: number]: BlockIndexMetaData;
+}
+interface ArticleMetaData {
+	[title: string]: BlockIndex;
+}
+interface UserMetaData {
+	[user: string]: ArticleMetaData;
+}
+
+const database: UserMetaData = {
+	"samples": {
+    "does-a-law-mean-what-it-says-or-what-it-meant": {
+			6: {
+				type: 'AMENDMENTS',
+				range: [369, 514]
+			},
+			9: {
+				type: 'AMENDMENTS',
+				range: [369, 442]
+			}
+		},
+		"dont-worry-well-protect-the-constitution-for-you": {
+			1: {
+				type: 'CONSTITUTION',
+				range: [19343, 19970]
+			}
+		}
+  }
+};
+
+export default database;
