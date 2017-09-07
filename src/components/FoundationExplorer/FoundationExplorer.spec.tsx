0ed7030115b716063a0d4fbaed6f8afadcdbdba2
Todd <todd@websbytodd.com>
2017-09-07 14:39:29 -0400

    Added tests for all stable components. Still more test cases to be added.

---
+++ src/components/FoundationExplorer/FoundationExplorer.spec.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { mount, ReactWrapper } from "enzyme";
+import FoundationExplorer from "./FoundationExplorer";
+import { FoundationTextType } from "../Foundation";
+
+const onBackClick = jest.fn();
+const onSetClick = jest.fn();
+
+let wrapper: ReactWrapper;
+
+beforeAll(() => {
+	let title = "does-a-law-mean-what-it-says-or-what-it-meant";
+	let user = "samples";
+	let blockIndex = 6;
+	let range: [number, number] = [30794, 30939];
+	let scrollTop = 101;
+	let type: FoundationTextType = "CONSTITUTION";
+	wrapper = mount(
+		<FoundationExplorer 
+			articleTitle={title} 
+			articleUser={user} 
+			blockIndex={blockIndex}
+			range={range}
+			scrollTop={scrollTop}
+			type={type}
+		/>
+	);
+});
+
+test("FoundationExplorer renders", () => {
+	expect(wrapper.find(FoundationExplorer).length).toBe(1);
+});
+
+// test("Set button works", () => {
+// 	wrapper.find('.editor__block--overlay').simulate('click');
+// 	expect(onSetClick).toHaveBeenCalled();
+// 	//Then highlight additional text and simulate click on '.document__text--selected'
+// });
+
+/**
+ * TODO: 
+ *   - Add the following test cases:
+ *     + 'Back button works'
+ *   - Fix test cases:
+ *     + 'Set button works' called window.location.href. Need to mock this out.
+ */
---
+++ src/components/MyTake/MyTake.spec.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { mount, ReactWrapper } from "enzyme";
+import MyTake, { MyTakeState } from "./MyTake";
+
+const onBackClick = jest.fn();
+const onSetClick = jest.fn();
+
+let wrapper: ReactWrapper;
+
+beforeAll(() => {
+	let initState: MyTakeState = {
+		takeDocument: {
+			title: "",
+			blocks: [{ kind: "paragraph", text: "" }]
+		},
+		activeBlockIndex: -1
+	}
+
+	wrapper = mount(
+		<MyTake 
+			initState={initState}
+		/>
+	);
+});
+
+test("MyTake renders", () => {
+	expect(wrapper.find(MyTake).length).toBe(1);
+});
+
+/**
+ * TODO: 
+ *   - Add the following test cases:
+ *     + 'When Document is clicked in FoundationExplorer, addDocument isCalledWith(type, range)'
+ *     + 'When addDocument is called, state is updated'
+ *     + 'When enter key is pressed, addParagraph isCalled)'
+ *     + 'When addParagraph is called, state is updated'
+ *     + 'When Video set button is clicked, addVideo isCalledWIth(id, range)'
+ *     + 'When addVideo is called, state is updated'
+ *     + 'removeParagraph is calledWith(idx) when '
+ *       - Paragraph is empty and loses focus
+ *       - Paragraph is empty and backspace or delete is pressed
+ *     + 'When removeParagraph is called, state is updated'
+ *     + 'When text key is pressed, handleTakeBlockChange is calledWidth(idx, value)'
+ *     + 'When handleTakeBlockChange is called, state is updated'
+ *     + 'When block gains focus, handleTakeBlockFocus is calledWith(idx)'
+ *     + 'When handleTakeBlockFocus is called, state('activeBlockIndex') is updated
+ */
