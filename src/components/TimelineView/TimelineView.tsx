864768ceea1b4c4b7c6b8bf04e23439efd4b9be8
Todd <todd@websbytodd.com>
2017-09-11 14:05:06 -0400

    Prettier.

--- src/components/TimelineView/TimelineView.tsx
+++ src/components/TimelineView/TimelineView.tsx
@@ -4,44 +4,47 @@ import Timeline from "../Timeline";
 import TimelinePreview, { PreviewDocument } from "../TimelinePreview";
 import { FoundationTextType } from "../Foundation";
 
-interface TimelineViewProps {
-
-}
+interface TimelineViewProps {}
 
 interface TimelineViewState {
-	previewDocument: PreviewDocument | null;
+  previewDocument: PreviewDocument | null;
 }
 
-export default class TimelineView extends React.Component<TimelineViewProps, TimelineViewState> {
+export default class TimelineView extends React.Component<
+  TimelineViewProps,
+  TimelineViewState
+> {
   constructor(props: TimelineViewProps) {
-		super(props);
-		this.state = {
-			previewDocument: null
-		}
-	}
-	showPreview = (range: [number, number], type: FoundationTextType, title: string) => {
-		console.log('HUZZAH!!');
-		this.setState({
-			previewDocument: {
-				range: range,
-				type: type,
-				title: title
-			}
-		})
-	}
-	render() {
+    super(props);
+    this.state = {
+      previewDocument: null
+    };
+  }
+  showPreview = (
+    range: [number, number],
+    type: FoundationTextType,
+    title: string
+  ) => {
+    console.log("HUZZAH!!");
+    this.setState({
+      previewDocument: {
+        range: range,
+        type: type,
+        title: title
+      }
+    });
+  };
+  render() {
     return (
       <div>
-        <Timeline 
-					onItemClick={this.showPreview}
-				/>
-				{this.state.previewDocument ? 
-					<TimelinePreview 
-						range={this.state.previewDocument.range} 
-						type={this.state.previewDocument.type} 
-						title={this.state.previewDocument.title} 
-					/> 
-				: null }
+        <Timeline onItemClick={this.showPreview} />
+        {this.state.previewDocument
+          ? <TimelinePreview
+              range={this.state.previewDocument.range}
+              type={this.state.previewDocument.type}
+              title={this.state.previewDocument.title}
+            />
+          : null}
       </div>
     );
   }
