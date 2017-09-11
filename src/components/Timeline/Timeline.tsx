657347e219975bded831a4383a9163034477dbef
Todd <todd@websbytodd.com>
2017-09-11 10:01:13 -0400

    Updates the database structure to have an array of DocumentExcerpts representing different sections of a long Document. For example, each Amendment is a part of the larger Constitution Document, so each Amendment is its own DocumentExcerpt, except for the Bill of Rights which is its own DocumentExcerpt.
    Also added an array of Videos to the database.
    
    Using this database to populate the Foundation timeline.

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import database from "../../utils/database";
 
 //import vis from "vis";
 let vis = require("vis");
@@ -11,40 +12,32 @@ interface VisDataItem {
   end?: string;
 }
 
-let items = new vis.DataSet([
-  // Using ratification dates
-  { id: 0, content: "United States Constitution", start: "1788-06-21" },
-  { id: 1, content: "Amendment 1", start: "1791-12-15" },
-  { id: 2, content: "Amendment 2", start: "1791-12-15" },
-  { id: 3, content: "Amendment 3", start: "1791-12-15" },
-  { id: 4, content: "Amendment 4", start: "1791-12-15" },
-  { id: 5, content: "Amendment 5", start: "1791-12-15" },
-  { id: 6, content: "Amendment 6", start: "1791-12-15" },
-  { id: 7, content: "Amendment 7", start: "1791-12-15" },
-  { id: 8, content: "Amendment 8", start: "1791-12-15" },
-  { id: 9, content: "Amendment 9", start: "1791-12-15" },
-  { id: 10, content: "Amendment 10", start: "1791-12-15" },
-  { id: 11, content: "Amendment 11", start: "1795-02-07" },
-  { id: 12, content: "Amendment 12", start: "1804-06-15" },
-  { id: 13, content: "Amendment 13", start: "1865-12-06" },
-  { id: 14, content: "Amendment 14", start: "1868-07-09" },
-  { id: 15, content: "Amendment 15", start: "1870-02-03" },
-  { id: 16, content: "Amendment 16", start: "1913-02-03" },
-  { id: 17, content: "Amendment 17", start: "1913-04-08" },
-  { id: 18, content: "Amendment 18", start: "1919-01-16" },
-  { id: 19, content: "Amendment 19", start: "1920-08-18" },
-  { id: 20, content: "Amendment 20", start: "1933-01-23" },
-  { id: 21, content: "Amendment 21", start: "1933-12-05" },
-  { id: 22, content: "Amendment 22", start: "1951-02-27" },
-  { id: 23, content: "Amendment 23", start: "1961-03-29" },
-  { id: 24, content: "Amendment 24", start: "1964-01-23" },
-  { id: 25, content: "Amendment 25", start: "1967-02-10" },
-  { id: 26, content: "Amendment 26", start: "1971-07-01" },
-  { id: 27, content: "Jimmy Carter - Gerald Ford #1", start: "1976-08-23" },
-  { id: 28, content: "Jimmy Carter - Gerald Ford #2", start: "1976-10-06" },
-  { id: 29, content: "Jimmy Carter - Gerald Ford #3", start: "1976-10-22" },
-  { id: 30, content: "Amendment 27", start: "1992-05-05" }
-]);
+let dataset: any = [];
+let idx = 0;
+
+database.videos.map(video => {
+  dataset = [
+    ...dataset,
+    {
+      id: idx++,
+      start: video.date,
+      content: video.title
+    }
+  ];
+});
+
+database.excerpts.map(excerpt => {
+  dataset = [
+    ...dataset,
+    {
+      id: idx++,
+      start: excerpt.date,
+      content: excerpt.name
+    }
+  ];
+});
+
+let items = new vis.DataSet(dataset);
 
 class ItemTemplate extends React.Component<{ item: any }, {}> {
   render() {
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -16,20 +16,232 @@ interface User {
   articles: Article[];
 }
 
-interface Database {
-  documents: Document[];
-  users: User[];
-}
-
 interface Document {
   type: FoundationTextType;
   heading: string;
+  last_updated: Date;
 }
+
+interface DocumentExcerpt {
+  name: string;
+  document: FoundationTextType;
+  highlightedRange: [number, number];
+  viewRange?: [number, number];
+  date: string;
+}
+
+interface Video {
+  id: string;
+  title: string;
+  date: string;
+}
+
+interface Database {
+  documents: Document[];
+  videos: Video[];
+  excerpts: DocumentExcerpt[];
+  users: User[];
+}
+
 const database: Database = {
   documents: [
     {
       type: "CONSTITUTION",
-      heading: "Constitution for the United States of America"
+      heading: "Constitution for the United States of America",
+      last_updated: new Date("1992-05-05")
+    }
+  ],
+  videos: [
+    {
+      id: "qAqIKybNO38",
+      title: "Jimmy Carter - Gerald Ford #1",
+      date: "1976-09-23"
+    },
+    {
+      id: "TjHjU0Eu26Y",
+      title: "Jimmy Carter - Gerald Ford #2",
+      date: "1976-10-06"
+    },
+    {
+      id: "CipT04S0bVE",
+      title: "Jimmy Carter - Gerald Ford #3",
+      date: "1976-10-22"
+    }
+  ],
+  excerpts: [
+    {
+      name: "United States Constitution",
+      document: "CONSTITUTION",
+      highlightedRange: [0, 30412],
+      date: "1788-06-21"
+    },
+    {
+      name: "Bill of Rights",
+      document: "CONSTITUTION",
+      highlightedRange: [30434, 33609],
+      date: "1791-12-15"
+    },
+    // {
+    // 	name: "Amendment 1",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [30434, 30739],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 2",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [30761, 30939],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 3",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [30961, 31150],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 4",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [31172, 31537],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 5",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [31559, 32181],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 6",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [32203, 32714],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 7",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [32736, 33041],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 8",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [33063, 33207],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 9",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [33229, 33392],
+    // 	date: "1791-12-15"
+    // },
+    // {
+    // 	name: "Amendment 10",
+    // 	document: "CONSTITUTION",
+    // 	highlightedRange: [33414, 33609],
+    // 	date: "1791-12-15"
+    // },
+    {
+      name: "Amendment 11",
+      document: "CONSTITUTION",
+      highlightedRange: [33631, 33903],
+      date: "1795-02-07"
+    },
+    {
+      name: "Amendment 12",
+      document: "CONSTITUTION",
+      highlightedRange: [33925, 36297],
+      date: "1804-06-15"
+    },
+    {
+      name: "Amendment 13",
+      document: "CONSTITUTION",
+      highlightedRange: [36319, 36680],
+      date: "1865-12-06"
+    },
+    {
+      name: "Amendment 14",
+      document: "CONSTITUTION",
+      highlightedRange: [36702, 39402],
+      date: "1868-07-09"
+    },
+    {
+      name: "Amendment 15",
+      document: "CONSTITUTION",
+      highlightedRange: [39424, 39766],
+      date: "1870-02-03"
+    },
+    {
+      name: "Amendment 16",
+      document: "CONSTITUTION",
+      highlightedRange: [39788, 40015],
+      date: "1913-02-03"
+    },
+    {
+      name: "Amendment 17",
+      document: "CONSTITUTION",
+      highlightedRange: [40037, 40914],
+      date: "1913-04-08"
+    },
+    {
+      name: "Amendment 18",
+      document: "CONSTITUTION",
+      highlightedRange: [40936, 41727],
+      date: "1919-01-16"
+    },
+    {
+      name: "Amendment 19",
+      document: "CONSTITUTION",
+      highlightedRange: [41749, 42000],
+      date: "1920-08-18"
+    },
+    {
+      name: "Amendment 20",
+      document: "CONSTITUTION",
+      highlightedRange: [42022, 44106],
+      date: "1933-01-23"
+    },
+    {
+      name: "Amendment 21",
+      document: "CONSTITUTION",
+      highlightedRange: [44128, 44796],
+      date: "1933-12-05"
+    },
+    {
+      name: "Amendment 22",
+      document: "CONSTITUTION",
+      highlightedRange: [44818, 45818],
+      date: "1951-02-27"
+    },
+    {
+      name: "Amendment 23",
+      document: "CONSTITUTION",
+      highlightedRange: [45840, 46659],
+      date: "1961-03-29"
+    },
+    {
+      name: "Amendment 24",
+      document: "CONSTITUTION",
+      highlightedRange: [46681, 47170],
+      date: "1964-01-23"
+    },
+    {
+      name: "Amendment 25",
+      document: "CONSTITUTION",
+      highlightedRange: [47192, 49705],
+      date: "1967-02-10"
+    },
+    {
+      name: "Amendment 26",
+      document: "CONSTITUTION",
+      highlightedRange: [49727, 50066],
+      date: "1971-07-01"
+    },
+    {
+      name: "Amendment 27",
+      document: "CONSTITUTION",
+      highlightedRange: [50088, 50285],
+      date: "1992-05-05"
     }
   ],
   users: [
