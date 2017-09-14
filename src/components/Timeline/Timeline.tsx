8ce33fe8704e37c434466dcdb17346430b54051d
Todd <todd@websbytodd.com>
2017-09-14 12:46:08 -0400

    Added vis typings. Fill Timeline with items from the database. When an item is clicked, pass its ID up to the TImelineView component.

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -2,83 +2,103 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import database from "../../utils/database";
 import { FoundationTextType } from "../Foundation";
-
-//import vis from "vis";
-let vis = require("vis");
-
-interface VisDataItem {
-  id: number;
-  content: string;
-  start: string;
-  end?: string;
-}
-
-let dataset: any = [];
-let idx = 0;
-
-database.videos.map(video => {
-  dataset = [
-    ...dataset,
-    {
-      idx: idx++,
-      id: video.id,
-      title: video.title,
-      start: video.date,
-      content: video.title
-    }
-  ];
-});
-
-database.excerpts.map(excerpt => {
-  dataset = [
-    ...dataset,
-    {
-      idx: idx++,
-      start: excerpt.date,
-      content: excerpt.title,
-      document: excerpt.document,
-      range: excerpt.highlightedRange
-    }
-  ];
-});
+import { slugify } from "../../utils/functions";
+import * as vis from "vis";
 
 interface TimelineItemProps {
-  item: any;
-  onClick: (
-    range: [number, number],
-    type: FoundationTextType,
-    title: string
-  ) => void;
+  item: vis.DataItem;
 }
 
 class TimelineItem extends React.Component<TimelineItemProps, {}> {
   constructor(props: TimelineItemProps) {
     super(props);
   }
-  handleClick = () => {
-    console.log("YES!!! Clicked item");
-    let { item } = this.props.item;
-    this.props.onClick(item.range, item.document, item.content);
-  };
   render() {
     var { item } = this.props;
     return (
-      <div className="timeline__item" onClick={this.handleClick}>
-        <label onClick={this.handleClick}>
+      <div className="timeline__item">
+        <label>
           {item.content}
         </label>
-        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
 }
 
+interface TimelineData {
+  id: string;
+  start: Date;
+  content: string;
+}
+
+interface TimelineSelectEventHandlerProps {
+  items: TimelineData["id"][];
+  event: Event;
+}
+
+// More complete than vis.TimelineOptions
+interface TimelineOptions {
+  align?: string;
+  autoResize?: boolean;
+  clickToUse?: boolean;
+  configure?: vis.TimelineOptionsConfigureType;
+  dataAttributes?: vis.TimelineOptionsDataAttributesType;
+  editable?: vis.TimelineOptionsEditableType;
+  end?: vis.DateType;
+  format?: any; // TODO
+  groupEditable?: vis.TimelineOptionsGroupEditableType;
+  groupOrder?: vis.TimelineOptionsGroupOrderType;
+  groupOrderSwap?: vis.TimelineOptionsGroupOrderSwapFunction;
+  groupTemplate?(item?: any, element?: any, data?: any): any;
+  height?: vis.HeightWidthType;
+  hiddenDates?: any; // TODO
+  horizontalScroll?: boolean;
+  itemsAlwaysDraggable?: boolean;
+  locale?: string;
+  locales?: any; // TODO
+  moment?(): void; // TODO
+  margin?: vis.TimelineOptionsMarginType;
+  max?: vis.DateType;
+  maxHeight?: vis.HeightWidthType;
+  maxMinorChars?: number;
+  min?: vis.DateType;
+  minHeight?: vis.HeightWidthType;
+  moveable?: boolean;
+  multiselect?: boolean;
+  multiselectPerGroup?: boolean;
+  onAdd?(): void; // TODO
+  onAddGroup?(): void; // TODO
+  onUpdate?(): void; // TODO
+  onMove?(): void; // TODO
+  onMoveGroup?(): void; // TODO
+  onMoving?(): void; // TODO
+  onRemove?(): void; // TODO
+  onRemoveGroup?(): void; // TODO
+  order?(a: TimelineData, b: TimelineData): number;
+  orientation?: vis.TimelineOptionsOrientationType;
+  rollingMode?: any;
+  selectable?: boolean;
+  showCurrentTime?: boolean;
+  showMajorLabels?: boolean;
+  showMinorLabels?: boolean;
+  stack?: boolean;
+  snap?: vis.TimelineOptionsSnapFunction;
+  start?: vis.DateType;
+  template?(item?: any, element?: any, data?: any): any;
+  throttleRedraw?: number;
+  timeAxis?: vis.TimelineTimeAxisOption;
+  type?: string;
+  tooltipOnItemUpdateTime?: boolean | { template(item: any): any };
+  verticalScroll?: boolean;
+  width?: vis.HeightWidthType;
+  zoomable?: boolean;
+  zoomKey?: string;
+  zoomMax?: number;
+  zoomMin?: number;
+}
+
 interface TimelineProps {
-  onItemClick: (
-    range: [number, number],
-    type: FoundationTextType,
-    title: string
-  ) => void;
+  onItemClick: (excerptId: string) => void;
 }
 
 interface TimelineState {}
@@ -87,39 +107,74 @@ export default class Timeline extends React.Component<
   TimelineProps,
   TimelineState
 > {
-  private timeline: any;
+  private timeline: vis.Timeline;
+  private timelineData: TimelineData[] = [];
   constructor(props: TimelineProps) {
     super(props);
   }
   initTimeline = () => {
+    for (let video of database.videos) {
+      this.timelineData = [
+        ...this.timelineData,
+        {
+          id: video.id,
+          start: video.primaryDate,
+          content: video.title
+        }
+      ];
+    }
+
+    for (let excerpt of database.excerpts) {
+      let idx = slugify(excerpt.title);
+      this.timelineData = [
+        ...this.timelineData,
+        {
+          id: idx,
+          start: excerpt.primaryDate,
+          content: excerpt.title
+        }
+      ];
+    }
+
     let container = document.getElementById("mytimeline");
-    let items = new vis.DataSet(dataset);
-    let options = {
+    let items: vis.DataSet<TimelineData> = new vis.DataSet(this.timelineData);
+    let options: TimelineOptions = {
       orientation: "top",
       start: new Date(1770, 0, 1),
       end: new Date(),
       order: this.orderById,
-      template: (item: any, element: any) => {
+      template: (item: TimelineData, element: HTMLElement) => {
         if (!item) {
           return;
         }
         ReactDOM.unmountComponentAtNode(element);
-        return ReactDOM.render(
-          <TimelineItem item={item} onClick={this.props.onItemClick} />,
-          element
-        );
+        return ReactDOM.render(<TimelineItem item={item} />, element);
       }
     };
-    this.timeline = new vis.Timeline(container, items, options);
+    if (container) {
+      this.timeline = new vis.Timeline(
+        container,
+        items,
+        options as vis.TimelineOptions
+      );
+    }
   };
-  orderById = (a: VisDataItem, b: VisDataItem) => {
-    return a.id - b.id;
+  orderById = (a: TimelineData, b: TimelineData): number => {
+    if (a.id < b.id) {
+      return -1;
+    }
+    return 1;
+  };
+  handleClick = (properties: TimelineSelectEventHandlerProps) => {
+    let dataSetId = properties.items[0];
+    let dataItem = this.timelineData.filter((dataItem: any) => {
+      return dataItem.id === dataSetId;
+    })[0];
+    this.props.onItemClick(dataItem.id);
   };
   componentDidMount() {
     this.initTimeline();
-    // this.timeline.on('select', function (properties: any) {
-    // 	console.log('Selected', properties);
-    // });
+    this.timeline.on("select", this.handleClick);
   }
   render() {
     return (
