8d9710d5b14f294357bb8f955ef255971e975092
Todd <todd@websbytodd.com>
2017-09-19 13:25:49 -0400

    Handle videos too.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import * as ReactDOM from "react-dom";
 import MyTake from "./components/MyTake";
 import config from "./components/MyTake/config";
 import { FoundationTextType } from "./components/Foundation";
-import { validators } from "./utils/functions";
-
+import { getFact, validators } from "./utils/functions";
+import { isDocument, isVideo } from "./utils/database";
 const CONSTITUTION: FoundationTextType = "CONSTITUTION";
 
 let app = document.getElementById("app");
@@ -19,24 +19,42 @@ if (app && app.hasAttribute("data-init")) {
 } else if (window.location.hash) {
   // Expect hash URL to be like, #{FoundationType}&{highlightRangeStart}&{highlightRangeEnd}&{URL of Take being read}
   // localhost:3000/new-take/#amendments&369&514&/samples/does-a-law-mean-what-it-says-or-what-it-meant/
-  let hashes = window.location.hash.toLowerCase().split("&");
+  let hashes = window.location.hash.split("&");
   let excerptId = hashes[0].substring(1);
   let range = [parseInt(hashes[1]), parseInt(hashes[2])];
   //let article = hashes[3];
 
-  let kind = "document";
-  if (!isNaN(range[0]) && !isNaN(range[1])) {
-    initJson = {
-      takeDocument: {
-        title: "",
-        blocks: [
-          // Currently only works for Foundation documents, need to add a video case
-          { kind: kind, excerptId: excerptId, highlightedRange: range },
-          { kind: "paragraph", text: "" }
-        ]
-      },
-      activeBlockIndex: -1
-    };
+  let fact = getFact(excerptId);
+  if (isDocument(fact)) {
+    if (!isNaN(range[0]) && !isNaN(range[1])) {
+      initJson = {
+        takeDocument: {
+          title: "",
+          blocks: [
+            { kind: "document", excerptId: excerptId, highlightedRange: range },
+            { kind: "paragraph", text: "" }
+          ]
+        },
+        activeBlockIndex: -1
+      };
+    } else {
+      initJson = config.initialState;
+    }
+  } else if (isVideo(fact)) {
+    if (!isNaN(range[0]) && !isNaN(range[1])) {
+      initJson = {
+        takeDocument: {
+          title: "",
+          blocks: [
+            { kind: "video", videoId: excerptId, range: range },
+            { kind: "paragraph", text: "" }
+          ]
+        },
+        activeBlockIndex: -1
+      };
+    } else {
+      initJson = config.initialState;
+    }
   } else {
     initJson = config.initialState;
   }
