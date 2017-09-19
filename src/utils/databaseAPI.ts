c55e73d3aec4bac39226bbd58a2b5ef046700765
Todd <todd@websbytodd.com>
2017-09-19 16:28:06 -0400

    Initial commit of the interface between the View (React) and the Model (Database).
    Probably took the coding a bit too far here because a lot of this logic will be handled by the database we choose. Until we have a database, we will likely never call "deleteArticle" for example. This is the Controller piece of the MVC that can't be implemented until we have a real Model.

---
+++ src/utils/databaseAPI.ts
@@ -0,0 +1,285 @@
+import database, { Database, Video, DocumentExcerpt, User, Article } from "./database";
+import { slugify } from "./functions";
+import { TakeBlock } from "../components/BlockEditor";
+
+export function getAllVideos(): Video[] {
+	return database.videos;
+}
+
+export function getVideo(videoId: string): Video | null {
+	for (let video of database.videos) {
+		if (video.id === videoId) {
+			return video;
+		}
+	}
+
+	return null;
+}
+
+export function getVideoTitle(videoId: string): string | null {
+	let video = getVideo(videoId);
+	if (video) {
+		return video.title;
+	}
+
+	return null;
+}
+
+export function getVideoPrimaryDate(videoId: string): Date | null {
+	let video = getVideo(videoId);
+	if (video) {
+		return video.primaryDate;
+	}
+
+	return null;
+}
+
+export function getVideoPrimaryDateKind(videoId: string): string | null {
+	let video = getVideo(videoId);
+	if (video) {
+		return video.primaryDateKind;
+	}
+
+	return null;
+}
+
+export function getAllDocumentExcerpts(): DocumentExcerpt[] {
+	return database.excerpts;
+}
+
+export function getDocumentExcerpt(excerptId: string): DocumentExcerpt | null {
+	for (let excerpt of database.excerpts) {
+		if (slugify(excerpt.title) === excerptId) {
+			return excerpt;
+		}
+	}
+
+	return null;
+}
+
+export function getDocumentExcerptTitle(excerptId: string): string | null {
+	for (let excerpt of database.excerpts) {
+		if (slugify(excerpt.title) === excerptId) {
+			return excerpt.title;
+		}
+	}
+
+	return null;
+}
+
+export function getDocumentExcerptFilename(excerptId: string): string | null {
+	for (let excerpt of database.excerpts) {
+		if (slugify(excerpt.title) === excerptId) {
+			return excerpt.filename;
+		}
+	}
+
+	return null;
+}
+
+export function getDocumentExcerptPrimaryDate(excerptId: string): Date | null {
+	for (let excerpt of database.excerpts) {
+		if (slugify(excerpt.title) === excerptId) {
+			return excerpt.primaryDate;
+		}
+	}
+
+	return null;
+}
+
+export function getDocumentExcerptPrimaryDateKind(excerptId: string): string | null {
+	for (let excerpt of database.excerpts) {
+		if (slugify(excerpt.title) === excerptId) {
+			return excerpt.primaryDateKind;
+		}
+	}
+
+	return null;
+}
+
+export function getUser(username: string): User | null {
+	for (let user of database.users) {
+		if (user.name === username) {
+			return user;
+		}
+	}
+	
+	return null;
+}
+
+export function createUser(username: string): boolean {
+	if (getUser(username)) {
+		return false;
+	}
+
+	return (database.users.push({
+		name: username,
+		articles: null
+	}) > 0)
+}
+
+export function updateUserName(oldUserName: string, newUserName: string): boolean {
+	let oldUser = getUser(oldUserName)
+	let newUser = getUser(newUserName);
+	if (!oldUser || newUser) {
+		return false;
+	}
+
+	oldUser.name = newUserName;
+
+	return true;
+}
+
+export function deleteUser(username: string): boolean {
+	if (!getUser(username)) {
+		// User doesn't exist
+		return false;
+	}
+
+	let newDatabase: Database = {
+		...database,
+		users: database.users.filter(user => {
+			return user.name !== username;
+		})
+	};
+
+	return true;
+}
+
+export function getAllArticles(username?: string): Article[] | null {
+	if (username) {
+		let user = getUser(username);
+		if (user) {
+			return user.articles;
+		} else {
+			return null;
+		}
+	}
+
+	let articles: Article[] = [];
+	for (let user of database.users) {
+		if (user.articles) {
+			articles = [
+				...articles,
+				...user.articles
+			]
+		}
+	}
+
+	return articles;
+}
+
+export function getArticle(username: string, titleSlug: string): Article | null {
+	let user = getUser(username);
+	if (user && user.articles) {
+		for (let article of user.articles) {
+			if (article.titleSlug === titleSlug) {
+				return article;
+			}
+		}
+	} else {
+		//Invalid username
+		return null;
+	}
+
+	//Article not found
+	return null;
+}
+
+export function createArticle(username: string, title: string, blocks: TakeBlock[]): boolean {
+	if (!title) {
+		// Throw error, title is required
+		return false;
+	}
+	if (getArticle(username, slugify(title))){
+		// Can't create duplicate article
+		return false;
+		/**
+		 * Could also create the article anyway and append an ID counter to the title,
+		 * e.g. new-take-title, new-take-title-2, new-take-title-3
+		 */
+	}
+	let user = getUser(username);
+	if (user) {
+		let articles = getAllArticles(username);
+		if (articles) {
+			articles = [
+				...articles,
+				{
+					title: title,
+					titleSlug: slugify(title),
+					blocks: blocks
+				}
+			]
+		} else {
+			articles = [
+				{
+					title: title,
+					titleSlug: slugify(title),
+					blocks: blocks
+				}
+			]
+		}
+		return true;
+	} else {
+		// Invalid username
+		return false;
+	}
+}
+
+export function updateArticleTitle(username: string, oldTitleSlug: string, newTitle: string): boolean {
+	let newArticle = getArticle(username, slugify(newTitle));
+	if (newArticle) {
+		// Can't create duplicate article
+		return false;
+		/**
+		 * Could also create the article anyway and append an ID counter to the title,
+		 * e.g. new-take-title, new-take-title-2, new-take-title-3
+		 */
+	}
+	
+	let oldArticle = getArticle(username, oldTitleSlug);
+	if (oldArticle) {
+		oldArticle.title = newTitle;
+		oldArticle.titleSlug = slugify(newTitle);
+
+		return true;
+	}
+
+	return false;
+}
+
+export function updateArticle(username: string, titleSlug: string, newBlocks: TakeBlock[]): boolean {
+	let article = getArticle(username, titleSlug);
+	if (article) {
+		article.blocks = newBlocks;
+
+		return true;
+	}
+
+	return false;
+}
+
+export function deleteArticle(username: string, titleSlug: string): boolean {
+	let user = getUser(username);
+	if (user && user.articles) {
+		let articles = user.articles.filter(article=>{
+			return article.titleSlug !== titleSlug;
+		})
+		let newUser: User = {
+			...user,
+			articles: articles
+		}
+		let newDatabase: Database = {
+			...database,
+			users: [
+				...database.users,
+				newUser
+			]
+		}
+		return true;
+	} else {
+		// User or articles don't exist
+		return false;
+	}
+}
