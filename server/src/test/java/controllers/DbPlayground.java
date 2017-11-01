2b763b0918f6d73fc3a98b7239ed5442585fa395
Ned Twigg <ned.twigg@diffplug.com>
2017-11-01 01:14:46 -0700

    Demo of the database API.

---
+++ server/src/test/java/controllers/DbPlayground.java
@@ -0,0 +1,76 @@
+/*
+ * MyTake.org
+ *
+ *  Copyright 2017 by its authors.
+ *  Some rights reserved. See LICENSE, https://github.com/mytake/mytake/graphs/contributors
+ */
+package controllers;
+
+import static db.Tables.CONFIRMACCOUNTLINK;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import common.JoobyDevRule;
+import db.tables.pojos.Confirmaccountlink;
+import db.tables.records.ConfirmaccountlinkRecord;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.jooq.DSLContext;
+import org.jooq.Result;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class DbPlayground {
+	@ClassRule
+	public static JoobyDevRule dev = JoobyDevRule.empty();
+
+	@Test
+	public void demo() {
+		// Always name the variable dsl, and use it in a try block.
+		// In the jOOQ manual, they name the variable `create`, but we should always name it `dsl`
+		// https://www.jooq.org/doc/3.10/manual/getting-started/the-manual/
+		try (DSLContext dsl = dev.dsl()) {
+			// the start of every query or modification is selecting a table
+			// there are two equivalent options:
+			//     A) db.Tables.EASYTRIAL
+			//     B) db.tables.Easytrial.EASYTRIAL
+			// always use A) with a static import, never use B), because B) collides with the POJO names (will make sense later)
+			assertThat(dsl.fetch(CONFIRMACCOUNTLINK)).isEmpty();
+
+			// for every table, there are two different classes that represent a row:
+			//     A) db.tables.records.ConfirmaccountlinkRecord - has a connection to the DB, for in-place update, delete, etc.
+			//     B) db.tables.pojos.Confirmaccountlink         - just a plain POJO
+			ConfirmaccountlinkRecord inserted = dsl.newRecord(CONFIRMACCOUNTLINK);
+			inserted.setCode("should be 44 long");
+			inserted.setCreatedAt(dev.time().nowTimestamp());
+			inserted.setExpiresAt(dev.time().nowTimestamp().plus(10, TimeUnit.MINUTES));
+			inserted.setRequestorIp("192.168.0.1");
+			inserted.setUsername("user");
+			inserted.setEmail("user@email.com");
+			inserted.insert();
+
+			ConfirmaccountlinkRecord fetched = dsl.fetchOne(CONFIRMACCOUNTLINK);
+			assertThat(fetched.getEmail()).isEqualTo("user@email.com");
+			assertThat(fetched.getCode()).isEqualTo("should be 44 long                           ");
+
+			// these records maintain database connections
+			fetched.setEmail("other@email.com");
+			fetched.store();
+			inserted.refresh();
+			assertThat(inserted.getEmail()).isEqualTo("other@email.com");
+
+			// because they have these database objects, it's a bad idea to pass
+			// them into templatesm=, which is what the POJO objects are for
+			Confirmaccountlink pojoFromRecord = inserted.into(Confirmaccountlink.class);
+			pojoFromRecord.setEmail("another@email.com");
+			ConfirmaccountlinkRecord recordFromPojo = dsl.newRecord(CONFIRMACCOUNTLINK, pojoFromRecord);
+			assertThat(recordFromPojo.getEmail()).isEqualTo("another@email.com");
+
+			// fetching multiple things works with pojos as well
+			Result<ConfirmaccountlinkRecord> result = dsl.fetch(CONFIRMACCOUNTLINK);
+			List<Confirmaccountlink> pojos = result.into(Confirmaccountlink.class);
+			assertThat(pojos).hasSize(1);
+		}
+	}
+}
