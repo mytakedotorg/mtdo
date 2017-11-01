b6f85724c9dc0edc5cdc3a4e93780add97eefdfd
Ned Twigg <ned.twigg@diffplug.com>
2017-10-31 23:52:34 -0700

    Added the database connection.
    - Prod gets its database connection from Heroku env variables
    - `gradlew runProd` can set this variable from `~/.gradle/gradle.properties` for a local setup
    - Dev spins up an empty local postgres, then applies all the migration scripts

--- server/src/main/java/common/Prod.java
+++ server/src/main/java/common/Prod.java
@@ -8,7 +8,10 @@ package common;
 
 import java.security.SecureRandom;
 import java.util.Random;
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
 import org.jooby.Jooby;
+import org.jooby.jooq.jOOQ;
 
 /**
  * The app that we run in production.  See {@link Dev} in the test
@@ -23,6 +26,16 @@ public class Prod extends Jooby {
 			binder.bind(Time.class).toInstance(() -> System.currentTimeMillis());
 		});
 		common(this);
+		use(new HerokuDatabase.Module());
+		use(new jOOQ());
+		use((env, conf, binder) -> {
+			env.onStart(registry -> {
+				Flyway flyway = new Flyway();
+				flyway.setDataSource(registry.require(DataSource.class));
+				flyway.setLocations("db/migration");
+				flyway.migrate();
+			});
+		});
 		controllers(this);
 	}
 
--- server/src/test/java/common/Dev.java
+++ server/src/test/java/common/Dev.java
@@ -6,8 +6,20 @@
  */
 package common;
 
+import com.diffplug.common.base.Errors;
+import com.google.inject.Binder;
+import com.opentable.db.postgres.embedded.DatabasePreparer;
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
+import com.opentable.db.postgres.embedded.FlywayPreparer;
+import com.opentable.db.postgres.embedded.PreparedDbProvider;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import org.jooby.Env;
 import org.jooby.Jooby;
+import org.jooby.jooq.jOOQ;
 
 /**
  * The app that we run in unit tests.  See {@link Prod} in the main
@@ -21,7 +33,39 @@ public class Dev extends Jooby {
 		});
 		use(new DevTime.Module());
 		Prod.common(this);
+		use(new EmbeddedPostgresModule());
+		use(new jOOQ());
 		Prod.controllers(this);
+	}
+
+	static class EmbeddedPostgresModule implements Jooby.Module {
+		EmbeddedPostgres postgres;
+
+		@Override
+		public Config config() {
+			try {
+				postgres = EmbeddedPostgres.builder()
+						.setCleanDataDirectory(true)
+						.start();
+
+				DatabasePreparer prep = FlywayPreparer.forClasspathLocation("db/migration");
+				PreparedDbProvider provider = PreparedDbProvider.forPreparer(prep);
+				String jdbcUrl = provider.createDatabase();
+
+				Map<String, String> map = new HashMap<>();
+				map.put("db.url", jdbcUrl);
+				map.put("db.user", "postgres");
+				map.put("db.password", "postgres");
+				return ConfigFactory.parseMap(map);
+			} catch (Exception e) {
+				throw Errors.asRuntime(e);
+			}
+		}
+
+		@Override
+		public void configure(Env env, Config conf, Binder binder) throws Throwable {
+			env.onStop(postgres::close);
+		}
 	}
 
 	public static void main(String[] args) {
