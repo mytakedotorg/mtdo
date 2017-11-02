39b91bef2c615efc681d1892ca1f617cfbfe7fd3
Ned Twigg <ned.twigg@diffplug.com>
2017-11-01 17:05:33 -0700

    Added utility methods for working with text.

---
+++ server/src/main/java/common/Text.java
@@ -0,0 +1,31 @@
+/*
+ * MyTake.org
+ *
+ *  Copyright 2017 by its authors.
+ *  Some rights reserved. See LICENSE, https://github.com/mytake/mytake/graphs/contributors
+ */
+package common;
+
+import java.util.Locale;
+import org.jooby.Request;
+
+/** Utility methods for text. */
+public class Text {
+	/** Returns the given param, lowercased. */
+	public static String lowercase(Request req, String param) {
+		return lowercase(req.param(param).value());
+	}
+
+	/** Lowercases the given input. */
+	public static String lowercase(String input) {
+		return input.toLowerCase(Locale.ROOT);
+	}
+
+	/** Slugify. */
+	public static String slugify(String input) {
+		String lowercase = lowercase(input);
+		return lowercase.replace(' ', '-') // replace spaces with hyphens
+				.replaceAll("[-]+", "-") // replace multiple hypens with a single hyphen
+				.replaceAll("[^\\w-]+", ""); // replace non-alphanumerics and non-hyphens
+	}
+}
