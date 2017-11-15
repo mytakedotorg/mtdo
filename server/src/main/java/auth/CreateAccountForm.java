fab4e1525b9f3cf7463e92bbc87c5df39307d56a
Ned Twigg <ned.twigg@diffplug.com>
2017-11-15 13:25:04 -0800

    Refactored ReservedUsernames out of CreateAccountForm.

--- server/src/main/java/auth/CreateAccountForm.java
+++ server/src/main/java/auth/CreateAccountForm.java
@@ -12,16 +12,12 @@ import static auth.AuthModule.REDIRECT;
 import static db.Tables.ACCOUNT;
 import static db.Tables.CONFIRMACCOUNTLINK;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import common.Emails;
 import common.RandomString;
-import common.RedirectException;
 import common.Text;
 import common.Time;
 import common.UrlEncodedPath;
-import controllers.AboutUs;
-import controllers.Foundation;
 import db.VarChars;
 import db.tables.pojos.Account;
 import db.tables.records.AccountRecord;
@@ -47,36 +43,9 @@ public class CreateAccountForm extends MetaFormDef.HandleValid {
 	private static final int FACEBOOK_MIN_USERNAME = 5;
 	private static final int FACEBOOK_MAX_USERNAME = 50;
 	private static final Pattern LOWERCASE_AND_DASH = Pattern.compile("[a-z\\d-]*");
+
 	private static final String msg_ALLOWED_CHARACTERS = "Can only use lowercase letters, numbers, and '-'";
-
-	public static final ImmutableSet<String> RESERVED_USERNAMES = ImmutableSet.of(
-			"drafts",			// drafts
-			"import",			// import from e.g. Google docs?
-			"api",				// for api access
-			"settings",			// for user settings
-			url(Foundation.URL),	// for serving evidence
-			"blog",				// for a blog
-			url(AboutUs.URL),	// for us
-			"legal",				// for legal attributes
-			url(AuthModule.URL_login),
-			url(AuthModule.URL_logout),
-			url(AuthModule.URL_confirm),
-			url(RedirectException.Module.URL_notFound),
-			url(RedirectException.Module.URL_badRequest));
-
 	private static final String msg_USERNAME_NOT_AVAILABLE = "Username is not available";
-
-	static String url(String url) {
-		Preconditions.checkArgument(url.startsWith("/"));
-		int nextSlash = url.indexOf('/', 1);
-		if (nextSlash == -1) {
-			return Text.lowercase(url.substring(1));
-		} else if (nextSlash == url.length() - 1) {
-			return Text.lowercase(url.substring(1, nextSlash));
-		} else {
-			throw new IllegalArgumentException("Must be just /url/ or /url, was " + url);
-		}
-	}
 
 	@Override
 	public Set<MetaField<?>> fields() {
@@ -119,7 +88,7 @@ public class CreateAccountForm extends MetaFormDef.HandleValid {
 	public boolean handleSuccessful(MetaFormValidation validation, Request req, Response rsp) throws Throwable {
 		String username = Text.lowercase(validation.parsed(CREATE_USERNAME));
 		String email = Text.lowercase(validation.parsed(CREATE_EMAIL));
-		if (RESERVED_USERNAMES.contains(username)) {
+		if (ReservedUsernames.isReserved(username)) {
 			validation.errorForField(msg_USERNAME_NOT_AVAILABLE);
 		} else {
 			try (DSLContext dsl = req.require(DSLContext.class)) {
