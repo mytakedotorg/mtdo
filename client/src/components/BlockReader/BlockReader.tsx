c57f3274396599d1607da15a0a1c73957d82b85d
Todd <todd@websbytodd.com>
2017-11-16 17:12:35 -0500

    Added a save button.

--- client/src/components/BlockReader/BlockReader.tsx
+++ client/src/components/BlockReader/BlockReader.tsx
@@ -7,7 +7,9 @@ interface BlockReaderProps {
 }
 
 interface BlockReaderState {
-  takeDocument?: TakeDocument;
+  takeDocument: TakeDocument;
+  draftId?: number;
+  lastRevId?: number;
 }
 
 class BlockReader extends React.Component<BlockReaderProps, BlockReaderState> {
@@ -40,12 +42,58 @@ class BlockReader extends React.Component<BlockReaderProps, BlockReaderState> {
       "&" +
       offset;
   };
+  handleSaveClick = () => {
+    const headers = new Headers();
+
+    headers.append("Accept", "application/json"); // This one is enough for GET requests
+    headers.append("Content-Type", "application/json"); // This one sends body
+
+    //TODO: enforce title length <= 255
+    const request: RequestInit = {
+      method: "POST",
+      credentials: "include",
+      headers: headers,
+      body: JSON.stringify({
+        title: this.state.takeDocument.title,
+        blocks: this.state.takeDocument.blocks
+      })
+    };
+
+    if (this.state.draftId && this.state.lastRevId) {
+      request.body.draftid = this.state.draftId;
+      request.body.lastrevid = this.state.lastRevId;
+    }
+
+    fetch("/drafts/save", request)
+      .then(function(response) {
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.indexOf("application/json") >= 0) {
+          return response.json();
+        }
+        throw new TypeError("Oops, we haven't got JSON!");
+      })
+      .then(function(json) {
+        this.setState({
+          draftId: json.draftid,
+          lastRevId: json.lastrevid
+        });
+      })
+      .catch(function(error) {
+        throw error;
+      });
+  };
   render() {
     const eventHandlers = {
       onDocumentClick: this.handleClick
     };
     return (
       <div>
+        <button
+          className="editor__button video__button--save"
+          onClick={this.handleSaveClick}
+        >
+          Save
+        </button>
         <BlockEditor
           takeDocument={(Object as any).assign({}, this.state.takeDocument)}
           eventHandlers={eventHandlers}
