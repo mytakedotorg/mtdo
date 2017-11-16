d7e400bc0a1b08afb5b321d68cb6c841de5d94ba
Ned Twigg <ned.twigg@diffplug.com>
2017-11-16 11:59:37 -0800

    User can now do "/foundation/anything" and it will work.

--- server/src/main/java/controllers/Foundation.java
+++ server/src/main/java/controllers/Foundation.java
@@ -17,6 +17,6 @@ public class Foundation implements Jooby.Module {
 	@Override
 	public void configure(Env env, Config conf, Binder binder) throws Throwable {
 		env.router().get(URL, () -> views.Placeholder.foundation.template());
+		env.router().get(URL + "/:anything", () -> views.Placeholder.foundation.template());
 	}
-
 }
