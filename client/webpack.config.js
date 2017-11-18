a0f1fdeb7afea03dcf5dd90df223bfbfd5c1c784
Ned Twigg <ned.twigg@diffplug.com>
2017-11-18 15:06:25 -0800

    Removed sourcemaps from the production asset pipeline - saves ~10MB.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  //devtool: "source-map",
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
