f4d921bfb521f169c5c70b5d0c241ae106cc01fc
Ned Twigg <ned.twigg@diffplug.com>
2017-11-27 13:07:07 -0800

    Added PublishResult.

---
+++ client/src/main/java/java2ts/PublishResult.java
@@ -0,0 +1,13 @@
+/*
+ * MyTake.org
+ *
+ *  Copyright 2017 by its authors.
+ *  Some rights reserved. See LICENSE, https://github.com/mytake/mytake/graphs/contributors
+ */
+package java2ts;
+
+@jsweet.lang.Interface
+public class PublishResult {
+	public String publishedUrl;
+	public boolean conflict;
+}
