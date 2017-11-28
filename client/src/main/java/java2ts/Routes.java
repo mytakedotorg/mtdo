472cdaaa1163411bc01a6e6ffedccfe795d2347d
Ned Twigg <ned.twigg@diffplug.com>
2017-11-28 12:51:18 -0800

    Example shared constants between Java (which templates can see) and Typescript.

---
+++ client/src/main/java/java2ts/Routes.java
@@ -0,0 +1,12 @@
+/*
+ * MyTake.org
+ *
+ *  Copyright 2017 by its authors.
+ *  Some rights reserved. See LICENSE, https://github.com/mytake/mytake/graphs/contributors
+ */
+package java2ts;
+
+@jsweet.lang.Interface
+public class Routes {
+	public static final String DRAFTS = "/drafts";
+}
