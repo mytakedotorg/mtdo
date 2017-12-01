73831f374676783bc968182514e3fe843ef15353
Todd <todd@websbytodd.com>
2017-12-01 17:15:34 -0500

    Add the /foundation route.

--- client/src/main/java/java2ts/Routes.java
+++ client/src/main/java/java2ts/Routes.java
@@ -16,6 +16,7 @@ public class Routes {
 	public static final String LOGIN = "/login";
 	public static final String LOGOUT = "/logout";
 
+	public static final String FOUNDATION = "/foundation";
 	public static final String FOUNDATION_DATA = "/foundation-data";
 	public static final String FOUNDATION_DATA_INDEX = "/foundation-data/index.json";
 }
