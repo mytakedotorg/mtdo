9c4d9c065697313c0d9d6e8f8f28a9c794e43be6
Todd <todd@websbytodd.com>
2017-12-02 11:06:21 -0500

    Added an error state/view for when a TimelinePreview fails to load.

--- client/src/main/typescript/components/TimelinePreviewContainer/TimelinePreviewContainer.tsx
+++ client/src/main/typescript/components/TimelinePreviewContainer/TimelinePreviewContainer.tsx
@@ -4,6 +4,7 @@ import { fetchFact, FoundationNode } from "../../utils/functions";
 import { isVideo, isDocument } from "../../utils/databaseData";
 import TimelinePreview, { Ranges, SetFactHandlers } from "../TimelinePreview";
 import TimelinePreviewLoadingView from "../TimelinePreviewLoadingView";
+import TimelinePreviewErrorView from "../TimelinePreviewErrorView";
 import { Foundation } from "../../java2ts/Foundation";
 import { Routes } from "../../java2ts/Routes";
 
@@ -15,6 +16,7 @@ interface TimelinePreviewContainerProps {
 }
 
 interface TimelinePreviewContainerState {
+  error: boolean;
   loading: boolean;
   nodes?: FoundationNode[];
 }
@@ -27,7 +29,8 @@ export default class TimelinePreviewContainer extends React.Component<
     super(props);
 
     this.state = {
-      loading: true
+      loading: true,
+      error: false
     };
   }
   getFact = (factHash: string) => {
@@ -39,26 +42,32 @@ export default class TimelinePreviewContainer extends React.Component<
           | Foundation.DocumentFactContent
           | Foundation.VideoFactContent
       ) => {
-        if (error) throw error;
-        let nodes: FoundationNode[] = [];
-
-        if (isDocument(factContent)) {
-          for (let documentComponent of factContent.components) {
-            nodes.push({
-              component: documentComponent.component,
-              innerHTML: [documentComponent.innerHTML],
-              offset: documentComponent.offset
-            });
-          }
-
+        if (error) {
           this.setState({
-            loading: false,
-            nodes: nodes
+            error: true,
+            loading: false
           });
-        } else if (isVideo(factContent)) {
-          throw "TODO";
         } else {
-          throw "Unknown kind of Fact";
+          let nodes: FoundationNode[] = [];
+
+          if (isDocument(factContent)) {
+            for (let documentComponent of factContent.components) {
+              nodes.push({
+                component: documentComponent.component,
+                innerHTML: [documentComponent.innerHTML],
+                offset: documentComponent.offset
+              });
+            }
+
+            this.setState({
+              loading: false,
+              nodes: nodes
+            });
+          } else if (isVideo(factContent)) {
+            throw "TODO";
+          } else {
+            throw "Unknown kind of Fact";
+          }
         }
       }
     );
@@ -68,21 +77,27 @@ export default class TimelinePreviewContainer extends React.Component<
   }
   componentWillReceiveProps(nextProps: TimelinePreviewContainerProps) {
     if (this.props.factLink.hash !== nextProps.factLink.hash) {
+      this.setState({
+        error: false,
+        loading: true
+      });
       this.getFact(nextProps.factLink.hash);
     }
   }
   render() {
     return (
       <div>
-        {this.state.loading || !this.state.nodes
-          ? <TimelinePreviewLoadingView />
-          : <TimelinePreview
-              factLink={this.props.factLink}
-              nodes={this.state.nodes}
-              setFactHandlers={this.props.setFactHandlers}
-              ranges={this.props.ranges}
-              offset={this.props.offset}
-            />}
+        {this.state.error
+          ? <TimelinePreviewErrorView />
+          : this.state.loading || !this.state.nodes
+            ? <TimelinePreviewLoadingView />
+            : <TimelinePreview
+                factLink={this.props.factLink}
+                nodes={this.state.nodes}
+                setFactHandlers={this.props.setFactHandlers}
+                ranges={this.props.ranges}
+                offset={this.props.offset}
+              />}
       </div>
     );
   }
