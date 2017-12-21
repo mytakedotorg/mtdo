04ecf93f1adbb35135507492262700f8902bb7ca
Ned Twigg <ned.twigg@diffplug.com>
2017-12-21 08:12:09 -0800

    OpenBrowser now exposes foundation assets in addition to styles and scripts.

--- server/src/test/java/common/OpenBrowser.java
+++ server/src/test/java/common/OpenBrowser.java
@@ -8,6 +8,7 @@ package common;
 
 import com.diffplug.common.base.Errors;
 import com.google.common.base.Preconditions;
+import controllers.FoundationAssets;
 import java.net.ServerSocket;
 import java.net.URI;
 import java.util.LinkedHashMap;
@@ -47,6 +48,8 @@ class OpenBrowser {
 
 			Jooby jooby = new Jooby();
 			CustomAssets.initTemplates(jooby);
+			jooby.use(new FoundationAssets());
+
 			jooby.port(port);
 			map.forEach((url, content) -> {
 				jooby.get(url, () -> content);
