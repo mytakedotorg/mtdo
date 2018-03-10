2219842aad74019cd311285cc4d20b2a51503c6e
Todd <todd@websbytodd.com>
2018-03-10 16:43:00 -0500

    Removed the user nav entirely. Keep the dropdown navigation menu at all screen sizes.

--- client/src/main/scripts/utils/window.ts
+++ client/src/main/scripts/utils/window.ts
@@ -6,35 +6,16 @@ import { Routes } from "../java2ts/Routes";
 
 const body = document.body;
 const loginDiv = document.getElementById("login");
-addNavEvents(loadUser());
-let userDropdown: Element | null;
-if (loginDiv && loginDiv.children[1]) {
-  userDropdown = loginDiv.children[1].children[0];
-} else {
-  userDropdown = null;
-}
+loadUser();
+addNavEvents();
 
-function addNavEvents(userNav?: boolean) {
+function addNavEvents() {
   const navToggle = document.getElementsByClassName("header__nav-toggle")[0];
   const navMenu = document.getElementsByClassName("nav")[0];
 
   function mainNavEvent(e: Event) {
-    if (
-      userDropdown &&
-      !userDropdown.classList.contains("header__dropdown--collapse")
-    ) {
-      toggleUserNav();
-    } else if (!e.defaultPrevented) {
+    if (!e.defaultPrevented) {
       toggleMainNav();
-    }
-    e.preventDefault();
-  }
-
-  function userNavEvent(e: Event) {
-    if (navMenu && !navMenu.classList.contains("collapse")) {
-      toggleMainNav();
-    } else if (!e.defaultPrevented) {
-      toggleUserNav();
     }
     e.preventDefault();
   }
@@ -50,20 +31,6 @@ function addNavEvents(userNav?: boolean) {
           !e.srcElement.classList.contains("nav__link-text")
         ) {
           toggleMainNav();
-          e.preventDefault();
-        }
-      }
-    } else if (
-      userDropdown &&
-      !userDropdown.classList.contains("header__dropdown--collapse") &&
-      !e.defaultPrevented
-    ) {
-      if (
-        e.srcElement &&
-        !e.srcElement.classList.contains("header__icon--login")
-      ) {
-        if (!e.srcElement.classList.contains("header__dropdown-link")) {
-          toggleUserNav();
           e.preventDefault();
         }
       }
@@ -86,22 +53,8 @@ function addNavEvents(userNav?: boolean) {
     }
   }
 
-  function toggleUserNav() {
-    if (userDropdown) {
-      if (userDropdown.classList.contains("header__dropdown--collapse")) {
-        userDropdown.classList.remove("header__dropdown--collapse");
-      } else {
-        userDropdown.classList.add("header__dropdown--collapse");
-      }
-    }
-  }
-
   if (navToggle) {
     navToggle.addEventListener("click", mainNavEvent);
-  }
-
-  if (userNav && loginDiv) {
-    loginDiv.children[0].addEventListener("click", userNavEvent);
   }
 
   body.addEventListener("click", bodyEvent);
@@ -124,11 +77,11 @@ function loadUser() {
   }
 
   const loginCookieStr = getCookieValue("loginui");
+  const navList = document.getElementById("nav-list");
+  const loginLink = document.getElementById("login-link");
   if (loginCookieStr) {
     // User is logged in
     const loginUi: LoginCookie = JSON.parse(JSON.parse(loginCookieStr));
-    const navList = document.getElementById("logout-container");
-    const loginLink = document.getElementById("login-link");
     if (navList && loginLink) {
       (loginLink as HTMLAnchorElement).href =
         "/" +
@@ -137,71 +90,23 @@ function loadUser() {
         Routes.PROFILE_TAB +
         "=" +
         Routes.PROFILE_TAB_EDIT;
+      loginLink.children[0].innerHTML = "Account/Profile";
+      const tabIndex = navList.children.length + 2;
       navList.innerHTML +=
-        '<li class="nav__list-item nav__list-item--bottom3">' +
-        '<a class="nav__link nav__link--bottom" href="' +
+        '<li class="nav__list-item nav__list-item--top">' +
+        '<a class="nav__link nav__link--top" href="' +
         Routes.LOGOUT +
-        '" tabindex="10">' +
-        '<span class="nav__link-text nav__link-text--bottom">Logout</span>' +
+        '" tabindex="' +
+        tabIndex +
+        '">' +
+        '<span class="nav__link-text nav__link-text--top">Logout</span>' +
         "</a>" +
         "</li>";
     } else {
       const msg = "window: navigation list and login link not found";
       throw msg;
     }
-    if (loginDiv) {
-      loginDiv.innerHTML =
-        '<a class="header__icon header__icon--login" href="#">' +
-        loginUi.username +
-        "</a>" +
-        '<div class="header__dropdown-container">' +
-        '<ul class="header__dropdown header__dropdown--collapse">' +
-        // Be sure to change SASS variable if you add or remove nav links (cd /client/src/main/styles && grep -Rn user-nav-items: *)
-        dropdown("New Draft", Routes.DRAFTS_NEW) +
-        dropdown("Drafts", Routes.DRAFTS) +
-        dropdown("Published", "/" + loginUi.username) +
-        dropdown(
-          "Stars",
-          "/" +
-            loginUi.username +
-            "?" +
-            Routes.PROFILE_TAB +
-            "=" +
-            Routes.PROFILE_TAB_STARS
-        ) +
-        dropdown(
-          "Profile",
-          "/" +
-            loginUi.username +
-            "?" +
-            Routes.PROFILE_TAB +
-            "=" +
-            Routes.PROFILE_TAB_EDIT
-        ) +
-        dropdown("Logout", Routes.LOGOUT) +
-        "</ul>" +
-        "</div>";
-      return true;
-    }
-  } else {
-    if (loginDiv) {
-      loginDiv.innerHTML =
-        '<a class="header__icon header__icon--login" href="' +
-        Routes.LOGIN +
-        '">Login</a>';
-    }
   }
-  return false;
-}
-
-function dropdown(text: string, link: string): string {
-  return (
-    '<li class="header__dropdown-list-item"><a class="header__dropdown-link" href="' +
-    link +
-    '">' +
-    text +
-    "</a></li>"
-  );
 }
 
 window.onerror = function(
