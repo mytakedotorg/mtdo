c0800205f92fba82f80161e05b236150035d0b56
Ned Twigg <ned.twigg@diffplug.com>
2018-03-31 14:34:52 -0700

    Fix lucene formatting error.

--- server/src/main/java/common/Lucene.java
+++ server/src/main/java/common/Lucene.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-
 import java2ts.Foundation.Speaker;
 import java2ts.Search;
 import java2ts.Search.FactResultList;
