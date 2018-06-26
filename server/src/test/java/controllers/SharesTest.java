d4e559d74177776db04ff7e5f758be191a3d9441
Todd <todd@websbytodd.com>
2018-06-26 14:48:19 -0400

    Added a simple test case.

---
+++ server/src/test/java/controllers/SharesTest.java
@@ -0,0 +1,35 @@
+/*
+ * MyTake.org
+ *
+ *  Copyright 2017 by its authors.
+ *  Some rights reserved. See LICENSE, https://github.com/mytakedotorg/mytakedotorg/graphs/contributors
+ */
+package controllers;
+
+import org.assertj.core.api.Assertions;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import common.JoobyDevRule;
+import common.JsonPost;
+import java2ts.Routes;
+import java2ts.Share;
+
+public class SharesTest {
+	@ClassRule
+	public static JoobyDevRule dev = JoobyDevRule.initialData();
+	
+	@Test
+	public void shareableURL() {
+		Share.ShareReq req = new Share.ShareReq();
+		req.title = "User generated title";
+		req.method = Share.METHOD_URL;
+		req.factSlug = "presidential-debate-clinton-dole-1-of-2";
+		req.highlightedRangeStart = "12.44";
+		req.highlightedRangeEnd = "23.15";
+		req.viewRangeStart = "11.369";
+		req.viewRangeEnd = "24.221";
+		Share.ShareRes res = JsonPost.post(dev.givenUser("samples"), req, Routes.API_SHARE, Share.ShareRes.class);
+		Assertions.assertThat(res).isNotNull();
+	}
+}
