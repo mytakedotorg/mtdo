76f5a846ada209661c832b5169268fba425c70d4
Ned Twigg <ned.twigg@diffplug.com>
2019-03-21 16:37:06 -0700

    Added a quick test for node.

--- node/src/main/scripts/app.spec.ts
+++ node/src/main/scripts/app.spec.ts
@@ -2,14 +2,16 @@ const underTest = require('./app');
 const request = require('supertest');
 const toMatchImageSnapshot = require('jest-image-snapshot');
 
-/*
 expect.extend({ toMatchImageSnapshot });
 
-describe("node.mytake.org smoketest", () => {
-    test('test for document', async () => {
-        const response = await request(app).get('someUrl');
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toMatchImageSnapshot();
-    });
-});
-*/
+test('show the 13th amendment', async () => {
+    const response = await request(underTest).get('/api/images/o_dRqrNJ62wzlgLilTrLxkHqGmvAS9qTpa4z4pjyFqA=_54-86_0-218.jpg');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toMatchImageSnapshot();
+})
+
+test('show some video captions', async () => {
+    const response = await request(underTest).get('/api/images/vrhLapmIbWECYassLC2Umf7Z16fusYgWWGhTP7KgIYU=_5839.620-5949.290.jpg');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toMatchImageSnapshot();
+})
