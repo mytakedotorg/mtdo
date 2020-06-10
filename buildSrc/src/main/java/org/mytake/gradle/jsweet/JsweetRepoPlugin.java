dbc2796f8a0d84eea60e181cbcd21a3ee119dca7
Ned Twigg <ned.twigg@diffplug.com>
2020-06-10 12:16:11 -0700

    Reduced jsweet's creep over the whole codebase.

---
+++ buildSrc/src/main/java/org/mytake/gradle/jsweet/JsweetRepoPlugin.java
@@ -0,0 +1,22 @@
+package org.mytake.gradle.jsweet;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.dsl.RepositoryHandler;
+
+/** Customize the jsweet transpilation process. */
+public class JsweetRepoPlugin implements Plugin<Project> {
+	@Override
+	public void apply(Project project) {
+		RepositoryHandler handler = project.getRepositories();
+		handler.add(handler.mavenCentral());
+		handler.add(handler.maven(jsweet -> {
+			jsweet.setUrl("http://repository.jsweet.org/artifactory/libs-release-local");
+			jsweet.setAllowInsecureProtocol(true);
+			jsweet.content(content -> {
+				content.includeGroup("org.jsweet");
+				content.includeGroup("org.jsweet.ext");
+			});
+		}));
+	}
+}
