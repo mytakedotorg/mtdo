d613925147113b01e001aa37b307aeff56e2d809
Ned Twigg <ned.twigg@diffplug.com>
2020-06-25 13:08:34 -0700

    Pur accurate copyrights and license headers on all code.

--- client/src/main/scripts/components/RangeContainer.spec.tsx
+++ client/src/main/scripts/components/RangeContainer.spec.tsx
@@ -1,3 +1,22 @@
+/*
+ * MyTake.org website and tooling.
+ * Copyright (C) 2018 MyTake.org, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact us at team@mytake.org
+ */
 import * as React from "react";
 import * as renderer from "react-test-renderer";
 import { TrackSliderEventHandlers } from "./TrackSlider";
