0284f83376e60044bbc9143b4352901d541b1e60
Todd <todd@websbytodd.com>
2020-07-30 14:31:07 -0400

    spotless

--- client/src/main/scripts/utils/foundationData/FoundationData.spec.ts
+++ client/src/main/scripts/utils/foundationData/FoundationData.spec.ts
@@ -1,6 +1,6 @@
 /*
  * MyTake.org website and tooling.
- * Copyright (C) 2018 MyTake.org, Inc.
+ * Copyright (C) 2018-2020 MyTake.org, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -17,6 +17,8 @@
  *
  * You can contact us at team@mytake.org
  */
+import { FoundationData } from "./FoundationData";
+
 test("TODO: Test FoundationData", async (done) => {
   expect(1).toBe(1);
   done();
