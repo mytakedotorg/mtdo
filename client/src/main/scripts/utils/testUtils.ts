62578e7003b5f966c8ffe29463baf167c64b6c5d
Todd <todd@websbytodd.com>
2020-08-01 11:55:33 -0400

    Still need this

--- client/src/main/scripts/utils/testUtils.ts
+++ client/src/main/scripts/utils/testUtils.ts
@@ -246,6 +246,37 @@ const videoFactFast: Foundation.VideoFactContent = {
   },
 };
 
+const timelineItems: TimelineItemData[] = [
+  {
+    id: "c7qu-ZE5SuipqSrOO30R3mnAA7K7nJ4fQ4zVIX0A2yg=",
+    idx: "c7qu-ZE5SuipqSrOO30R3mnAA7K7nJ4fQ4zVIX0A2yg=",
+    start: new Date("1788-06-21T00:00:00.000Z"),
+    content: "United States Constitution",
+    kind: "document",
+  },
+  {
+    id: "pMHhbW_I-wquOfoyPFAVQu8DMLMpYVxhGT8R1x71hYA=",
+    idx: "pMHhbW_I-wquOfoyPFAVQu8DMLMpYVxhGT8R1x71hYA=",
+    start: new Date("1791-12-15T00:00:00.000Z"),
+    content: "Bill of Rights",
+    kind: "document",
+  },
+  {
+    id: "-7DeOJAVJUsifUcIaZo7c41pol_guMxR6IEgYv28bHM=",
+    idx: "-7DeOJAVJUsifUcIaZo7c41pol_guMxR6IEgYv28bHM=",
+    start: new Date("1960-09-26T00:00:00.000Z"),
+    content: "John F. Kennedy - Nixon (1/4)",
+    kind: "video",
+  },
+  {
+    id: "bl03RovlxbTZK0yu25_VikP0Y2xSj-J9oFyGUTWIOZQ=",
+    idx: "bl03RovlxbTZK0yu25_VikP0Y2xSj-J9oFyGUTWIOZQ=",
+    start: new Date("1960-10-07T00:00:00.000Z"),
+    content: "John F. Kennedy - Nixon (2/4)",
+    kind: "video",
+  },
+];
+
 const documentFactLink: Foundation.FactLink = {
   fact: {
     title: "Amendment 13",
@@ -989,6 +1020,7 @@ export {
   logMeasurements,
   rangeStyle,
   takeDocument,
+  timelineItems,
   unzoomedRangeSliders,
   videoFactFast,
   videoFactLink,
