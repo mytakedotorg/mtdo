708cd8ecca6c6e85f364421c71a915fbd6f66c25
Todd <todd@websbytodd.com>
2020-08-03 16:47:16 -0400

    First cut at CearchHit

--- client/src/main/scripts/components/search/search.ts
+++ client/src/main/scripts/components/search/search.ts
@@ -22,6 +22,7 @@ import { FT } from "../../java2ts/FT";
 import { Routes } from "../../java2ts/Routes";
 import { Search } from "../../java2ts/Search";
 import { get } from "../../network";
+var bs = require("binary-search");
 
 export class SearchResult {
   constructor(
@@ -93,4 +94,120 @@ type HashesToTurns = Map<string, number[]>;
 interface VideoFactsToTurns {
   videoFact: FT.VideoFactContent;
   turns: number[];
+}
+
+interface SeachHitContent {
+  text: string;
+  isHighlighted: boolean;
+}
+
+class SearchHit {
+  constructor(
+    private highlightOffsets: Array<[number, number]>,
+    private hitOffsets: [number, number],
+    private turn: number,
+    private videoFact: FT.VideoFactContent
+  ) {}
+
+  getSpeaker(): string {
+    const { turn, videoFact } = this;
+    const fullName = videoFact.speakers[videoFact.speakerPerson[turn]].fullName;
+    return fullName.substring(fullName.lastIndexOf(" "));
+  }
+
+  getClipRange(): [number, number] {
+    const { hitOffsets, turn, videoFact } = this;
+    const veryFirstWord = videoFact.speakerWord[turn];
+    const firstChar = videoFact.charOffsets[veryFirstWord];
+    let firstWord = bs(
+      videoFact.charOffsets, // haystack
+      firstChar + hitOffsets[0], // needle
+      (element: number, needle: number) => {
+        return element - needle;
+      }
+    );
+
+    // usually the timestamp is between two words, in which case it returns (-insertionPoint - 2)
+    if (firstWord < 0) {
+      firstWord = -firstWord - 2;
+    }
+
+    const clipStart = videoFact.timestamps[firstWord];
+
+    let lastWord = bs(
+      videoFact.charOffsets, // haystack
+      firstChar + hitOffsets[1], // needle
+      (element: number, needle: number) => {
+        return element - needle;
+      }
+    );
+
+    // usually the timestamp is between two words, in which case it returns (-insertionPoint - 2)
+    if (lastWord < 0) {
+      lastWord = -lastWord - 2;
+    }
+
+    let clipEnd;
+    if (videoFact.timestamps[lastWord + 1]) {
+      clipEnd = videoFact.timestamps[lastWord + 1];
+    } else {
+      clipEnd = videoFact.timestamps[lastWord] + 2;
+    }
+
+    return [clipStart, clipEnd];
+  }
+
+  getContent(): SeachHitContent[] {
+    const searchHitContents: SeachHitContent[] = [];
+    const turnContent = this.getTurnContent();
+    let contentStartIdx = this.hitOffsets[0];
+    this.highlightOffsets.forEach((highlight) => {
+      const textBeforeHighlight = turnContent.substring(
+        contentStartIdx,
+        highlight[0]
+      );
+      const textOfHighlight = turnContent.substring(highlight[0], highlight[1]);
+      if (textBeforeHighlight) {
+        searchHitContents.push({
+          text: textBeforeHighlight,
+          isHighlighted: false,
+        });
+      }
+      if (textOfHighlight) {
+        searchHitContents.push({
+          text: textOfHighlight,
+          isHighlighted: true,
+        });
+      }
+      contentStartIdx = highlight[1];
+    });
+    const textAfterAllHighlights = turnContent.substring(
+      contentStartIdx,
+      this.hitOffsets[1]
+    );
+    if (textAfterAllHighlights) {
+      searchHitContents.push({
+        text: textAfterAllHighlights,
+        isHighlighted: false,
+      });
+    }
+    return searchHitContents;
+  }
+
+  private getTurnContent = (): string => {
+    const { turn, videoFact } = this;
+    let fullTurnText;
+    const firstWord = videoFact.speakerWord[turn];
+    const firstChar = videoFact.charOffsets[firstWord];
+
+    if (videoFact.speakerWord[turn + 1]) {
+      const lastWord = videoFact.speakerWord[turn + 1];
+      const lastChar = videoFact.charOffsets[lastWord] - 1;
+      fullTurnText = videoFact.plainText.substring(firstChar, lastChar);
+    } else {
+      // Result is in last turn
+      fullTurnText = videoFact.plainText.substring(firstChar);
+    }
+    return fullTurnText;
+  };
 }
