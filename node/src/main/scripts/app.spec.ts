87dd17468b2ec661c48b2f864b47918090cfb429
Ned Twigg <ned.twigg@diffplug.com>
2020-08-07 15:53:51 -0700

    Should always ask for png, not jpg.

--- node/src/main/scripts/app.spec.ts
+++ node/src/main/scripts/app.spec.ts
@@ -54,7 +54,7 @@ test("invalid fact should not hang", async (done) => {
     const consoleOutput: string[] = [];
     console.warn = (arg: any) => consoleOutput.push(arg.toString());
     const response = await request(underTest).get(
-      "/api/images/vrhLapmIbWECYassLC2Umf7Z16fusYgWWGhTP7KgIYU=_5839.620-5949.290.jpg"
+      "/api/images/vrhLapmIbWECYassLC2Umf7Z16fusYgWWGhTP7KgIYU=_5839.620-5949.290.png"
     );
     expect(response.statusCode).toBe(404);
     expect(consoleOutput.join("\n")).toMatchSnapshot();
