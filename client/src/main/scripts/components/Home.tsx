bb05a5cfbb39c4b39b56cf677c85cac3548b7be1
Todd <todd@websbytodd.com>
2020-08-11 13:14:30 -0400

    Useful text instead of lorem ipsum

--- client/src/main/scripts/components/Home.tsx
+++ client/src/main/scripts/components/Home.tsx
@@ -72,23 +72,10 @@ const Home: React.FC = () => {
       </HomeSection>
       <HomeSection containerClassName="hero">
         <div className="home__boxes">
+          <p>Use commas between search terms.</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Use a minus sign to exclude a search term. E.g. "wall, -wall street"
+            will include anything about a wall except for Wall Street.
           </p>
         </div>
       </HomeSection>
