6baa84282ce9f057a07e5ba52fce0a8fe3237b97
Ned Twigg <ned.twigg@diffplug.com>
2020-08-13 00:40:13 -0700

    We don't need our own `.d.ts` for binary-search anymore.

--- client/src/main/scripts/types/interfaces.d.ts
+++ client/src/main/scripts/types/interfaces.d.ts
@@ -1,6 +1,6 @@
 /*
  * MyTake.org website and tooling.
- * Copyright (C) 2017-2018 MyTake.org, Inc.
+ * Copyright (C) 2017-2020 MyTake.org, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -23,28 +23,6 @@ declare module "rc-slider";
 declare module "*.sbv" {
   const content: any;
   export default content;
-}
-//Typescript type definition for:
-//https://github.com/darkskyapp/binary-search
-declare module "binary-search" {
-  function binarySearch<A, B>(
-    haystack: A[],
-    needle: B,
-    comparator: (a: A, b: B, index?: number, haystack?: A[]) => any,
-    // Notes about comparator return value:
-    // * when a<b the comparator's returned value should be:
-    //   * negative number or a value such that `+value` is a negative number
-    //   * examples: `-1` or the string `"-1"`
-    // * when a>b the comparator's returned value should be:
-    //   * positive number or a value such that `+value` is a positive number
-    //   * examples: `1` or the string `"1"`
-    // * when a===b
-    //    * any value other than the return cases for a<b and a>b
-    //    * examples: undefined, NaN, 'abc'
-    low?: number,
-    high?: number
-  ): number; //returns index of found result or number < 0 if not found
-  export default binarySearch;
 }
 
 // https://github.com/niklasvh/base64-arraybuffer/pull/9
