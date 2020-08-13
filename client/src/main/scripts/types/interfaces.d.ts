96d9f18fbe4df958db82870ac2f1dd00baeee684
Ned Twigg <ned.twigg@diffplug.com>
2020-08-13 01:08:49 -0700

    Remove no-longer-needed String interface.

--- client/src/main/scripts/types/interfaces.d.ts
+++ client/src/main/scripts/types/interfaces.d.ts
@@ -25,11 +25,6 @@ declare module "*.sbv" {
   export default content;
 }
 
-declare interface String {
-  startsWith(search: string, pos?: number): boolean;
-  includes(searchString: string, pos?: number): boolean;
-}
-
 // This one is missing from @types/facebook-js-sdk. Declaration merging here.
 declare namespace facebook {
   interface InitParams {
