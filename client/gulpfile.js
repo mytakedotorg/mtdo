61603e74f9d0286303c500a30ae6b56f3c04d704
Ned Twigg <ned.twigg@diffplug.com>
2020-08-13 23:21:08 -0700

    Add a `socialEmbed` entry point and bundle to client, along with dummy SocialImage component.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -143,6 +143,7 @@ function webpackCfg(mode) {
     mode: mode === PROD ? "production" : "development",
     entry: {
       app: entryFor(mode, "/src/main/scripts/index.tsx"),
+      socialEmbed: entryFor(mode, "/src/main/scripts/socialEmbed.tsx"),
     },
     output: {
       filename: "[name].bundle.js",
@@ -165,10 +166,7 @@ function webpackCfg(mode) {
       rules: [
         {
           test: /\.tsx?$/,
-          exclude: [
-            /node_modules/,
-            __dirname + "/src/main/scripts/utils/drawVideoFact.ts",
-          ],
+          exclude: [/node_modules/],
           use:
             mode === PROD
               ? [{ loader: "awesome-typescript-loader" }]
---
+++ client/src/main/scripts/components/social/SocialImage.tsx
@@ -0,0 +1,40 @@
+/*
+ * MyTake.org website and tooling.
+ * Copyright (C) 2020 MyTake.org, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact us at team@mytake.org
+ */
+import * as React from "react";
+
+export type VideoTurn = {
+  kind: "videoTurn";
+  fact: string;
+  turn: number;
+  cut: [number, number];
+  highlights?: Array<[number, number]>;
+};
+
+export type SocialImageProps = {
+  embed: string;
+};
+
+export const SocialImage: React.FC<SocialImageProps> = (props) => {
+  return (
+    <div className="socialImage">
+      <p>{props.embed}</p>
+    </div>
+  );
+};
