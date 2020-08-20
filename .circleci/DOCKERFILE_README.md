7331dc6bb0e9355ef58618f51ac276721aa74ae1
Ned Twigg <ned.twigg@diffplug.com>
2020-08-20 16:15:47 -0700

    Update `nedtwigg/testrunner` to 1.3.0, fix for puppeteer - bump jdk from 8.0.252 to 8.0.262 - add deps for chrome headless

--- .circleci/DOCKERFILE_README.md
+++ .circleci/DOCKERFILE_README.md
@@ -9,10 +9,17 @@ sudo docker push nedtwigg/testrunner:${VERSION}
 ```
 
 # Changelog
+## [1.3.0] - 2020-08-20
+### Changed
+- from `cimg/openjdk:8.0.252-node` to `8.0.262-node`
+### Added
+- deps needed for headless Chrome
+
 ## [1.2.0] - 2020-05-06
 ### Changed
 - from `circleci/openjdk:8u252-node` to `cimg/openjdk:8.0.252-node`
 - reduced filesize impact of adding postgres client tools
+
 ## [1.1.0] - 2020-04-28
 ### Added
 - built-in node support
--- .circleci/install-packages.sh
+++ .circleci/install-packages.sh
@@ -19,6 +19,15 @@ sudo apt-get update
 # Install a new package, without unnecessary recommended packages:
 sudo apt-get -y install --no-install-recommends postgresql-client
 
+# Install dependencies of headless chrome
+# https://circleci.com/orbs/registry/orb/threetreeslight/puppeteer 0.1.2 (latest as of 2020-08-20)
+sudo apt-get install -yq \
+          gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 \
+          libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 \
+          libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 \
+          libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates \
+          fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
 # Delete cached files we don't need anymore:
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
