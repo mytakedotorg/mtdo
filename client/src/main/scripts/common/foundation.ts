06dcb2f41f69043601c340bb4cbcdc30ff043495
Todd <todd@websbytodd.com>
2020-08-27 10:51:56 -0400

    Revert "REVERT OR FORCE-PUSH OVER ME: a terrible test"
    This reverts commit a71776695af590457353ed56a4cb4f726b0e0998.

--- client/src/main/scripts/common/foundation.ts
+++ client/src/main/scripts/common/foundation.ts
@@ -21,7 +21,6 @@ import { FT } from "../java2ts/FT";
 import { Routes } from "../java2ts/Routes";
 import { get } from "../network";
 import { decodeVideoFact } from "./video";
-import { BookmarksClient } from "../components/bookmarks/bookmarks";
 
 export class Foundation {
   constructor(
@@ -93,38 +92,6 @@ export class FoundationFetcher {
   }
 
   async build(): Promise<Foundation> {
-    try {
-      const client = new BookmarksClient();
-      console.log("initial get");
-      console.log(await client.get());
-      console.log("initial add");
-      console.log(
-        await client.add([
-          {
-            fact: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            savedAt: "1981-01-01T00:00:00Z",
-            start: 2,
-            end: 718,
-          },
-        ])
-      );
-      console.log(await client.get());
-      console.log("initial remove");
-      console.log(
-        await client.remove([
-          {
-            fact: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            savedAt: "1981-01-01T00:00:00Z",
-            start: 2,
-            end: 718,
-          },
-        ])
-      );
-      console.log(await client.get());
-    } catch (err) {
-      console.warn(err);
-    }
-
     const facts = await Promise.all(
       this.requestedFacts.map((hash) => this.getFact(hash))
     );
