f58d3bfd7f9ee93bc792d7bdabb7754fb3eb0d73
Ned Twigg <ned.twigg@diffplug.com>
2020-09-10 16:13:35 -0700

    Add account columns for newsletter and typohardened usernames.

--- server/src/main/resources/db/migration/V10__username_optional.sql
+++ server/src/main/resources/db/migration/V10__username_optional.sql
@@ -1,6 +1,8 @@
 ALTER TABLE account ALTER COLUMN username DROP NOT NULL;
 ALTER TABLE account ADD COLUMN confirmed_at TIMESTAMP;
 ALTER TABLE account ADD COLUMN confirmed_ip INET;
-ALTER TABLE account ADD COLUMN newsletter BOOLEAN;
+ALTER TABLE account ADD COLUMN newsletter BOOLEAN NOT NULL DEFAULT TRUE;
+ALTER TABLE account ADD COLUMN username_typohard varchar(100) UNIQUE;
+
 DROP TABLE confirmaccountlink;
 ALTER TABLE loginlink ADD COLUMN redirect TEXT;
