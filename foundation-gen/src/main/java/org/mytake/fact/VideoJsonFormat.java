9484484108f10de20c005daafeca386249393fab
Ned Twigg <ned.twigg@diffplug.com>
2020-09-18 23:14:59 -0700

    Relicense foundation-gen from EPL to AGPLv3 with a linking exception (see #385).

--- foundation-gen/src/main/java/org/mytake/fact/VideoJsonFormat.java
+++ foundation-gen/src/main/java/org/mytake/fact/VideoJsonFormat.java
@@ -1,17 +1,33 @@
 /*
- * MyTake.org transcript GUI.
+ * MyTake.org website and tooling.
  * Copyright (C) 2020 MyTake.org, Inc.
- * 
- * The MyTake.org transcript GUI is licensed under EPLv2
- * because SWT is incompatible with AGPLv3, the rest of
- * MyTake.org is licensed under AGPLv3.
  *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with Eclipse SWT (or a modified version of that library), containing parts
+ * covered by the terms of the Eclipse Public License, the licensors of this Program
+ * grant you additional permission to convey the resulting work.
+ * {Corresponding Source for a non-source form of such a combination shall include the
+ * source code for the parts of Eclipse SWT used as well as that of the covered work.}
+ *
+ * You can contact us at team@mytake.org
  */
 package org.mytake.fact;
+
 
 import com.diffplug.common.base.Preconditions;
 import com.diffplug.common.io.ByteStreams;
