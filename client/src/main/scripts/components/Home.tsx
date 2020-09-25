cda927fbb68ab53dc9cb45267efb1724e6bc28be
Todd <todd@websbytodd.com>
2020-09-25 17:11:21 -0400

    split homepage sections into components

--- client/src/main/scripts/components/Home.tsx
+++ client/src/main/scripts/components/Home.tsx
@@ -52,27 +52,48 @@ const Home: React.FC = () => {
   }, []);
   return (
     <>
-      <HomeSection>
-        <h2 className="home__h1 home__h1--center">
-          Are we lying to you? Is&nbsp;this&nbsp;out&nbsp;of&nbsp;context?
-        </h2>
-        <p className="home__body home__body--center">Click one to see</p>
-        <div className="home__social-container">
-          <a href="/foundation/presidential-debate-clinton-trump-1-of-3/cut:!(2879,2891.639892578125),fact:'1L4K9lUrKC8dQBxDQTZeIxNEeKIgZjMPaA7SURzBljQ=',kind:videoCut">
-            {leftSocial}
-          </a>
-          <a href="/foundation/presidential-debate-clinton-trump-1-of-3/cut:!(2689.89990234375,2702.360107421875),fact:'1L4K9lUrKC8dQBxDQTZeIxNEeKIgZjMPaA7SURzBljQ=',kind:videoCut">
-            {rightSocial}
-          </a>
-        </div>
-        <h2 className="home__h1 home__h1--center">
-          Don't let someone else decide&nbsp;for&nbsp;you
-        </h2>
-        <p className="home__body home__body--center">
-          We're not tracking what you've said.
-          We're&nbsp;not&nbsp;selling&nbsp;your&nbsp;attention.
-        </p>
-      </HomeSection>
+      <WhatIsThis leftSocial={leftSocial} rightSocial={rightSocial} />
+      <HowToUseThis />
+      <GetInvolved />
+    </>
+  );
+};
+
+export default Home;
+
+interface WhatIsThisProps {
+  leftSocial: React.ReactElement;
+  rightSocial: React.ReactElement;
+}
+const WhatIsThis: React.FC<WhatIsThisProps> = (props) => {
+  return (
+    <HomeSection>
+      <h2 className="home__h1 home__h1--center">
+        Are we lying to you? Is&nbsp;this&nbsp;out&nbsp;of&nbsp;context?
+      </h2>
+      <p className="home__body home__body--center">Click one to see</p>
+      <div className="home__social-container">
+        <a href="/foundation/presidential-debate-clinton-trump-1-of-3/cut:!(2879,2891.639892578125),fact:'1L4K9lUrKC8dQBxDQTZeIxNEeKIgZjMPaA7SURzBljQ=',kind:videoCut">
+          {props.leftSocial}
+        </a>
+        <a href="/foundation/presidential-debate-clinton-trump-1-of-3/cut:!(2689.89990234375,2702.360107421875),fact:'1L4K9lUrKC8dQBxDQTZeIxNEeKIgZjMPaA7SURzBljQ=',kind:videoCut">
+          {props.rightSocial}
+        </a>
+      </div>
+      <h2 className="home__h1 home__h1--center">
+        Don't let someone else decide&nbsp;for&nbsp;you
+      </h2>
+      <p className="home__body home__body--center">
+        We're not tracking what you've said.
+        We're&nbsp;not&nbsp;selling&nbsp;your&nbsp;attention.
+      </p>
+    </HomeSection>
+  );
+};
+
+const HowToUseThis: React.FC = () => {
+  return (
+    <>
       <HomeSection
         containerClassName="home__section--dark"
         innerContainerClassName={"home__searchbar"}
@@ -126,35 +147,37 @@ const Home: React.FC = () => {
           </li>
         </ol>
       </HomeSection>
-      <HomeSection containerClassName="home__section--dark">
-        <h2 className="home__h2">Get involved</h2>
-        <div className="home__link-container">
-          <a className="home__link" href="https://meta.mytake.org/c/educators">
-            Forum for teachers
-          </a>
-          <a
-            className="home__link"
-            href="https://github.com/mytakedotorg/mytakedotorg/blob/staging/DEV_QUICKSTART.md"
-          >
-            GitHub repo for developers
-          </a>
-          <a
-            className="home__link"
-            href="https://meta.mytake.org/c/our-foundation"
-          >
-            Help us add more facts
-          </a>
-          <a className="home__link" href="https://meta.mytake.org/c/governance">
-            Donate money to keep it running
-          </a>
-        </div>
-      </HomeSection>
     </>
   );
 };
 
-export default Home;
-
+const GetInvolved: React.FC = () => {
+  return (
+    <HomeSection containerClassName="home__section--dark">
+      <h2 className="home__h2">Get involved</h2>
+      <div className="home__link-container">
+        <a className="home__link" href="https://meta.mytake.org/c/educators">
+          Forum for teachers
+        </a>
+        <a
+          className="home__link"
+          href="https://github.com/mytakedotorg/mytakedotorg/blob/staging/DEV_QUICKSTART.md"
+        >
+          GitHub repo for developers
+        </a>
+        <a
+          className="home__link"
+          href="https://meta.mytake.org/c/our-foundation"
+        >
+          Help us add more facts
+        </a>
+        <a className="home__link" href="https://meta.mytake.org/c/governance">
+          Donate money to keep it running
+        </a>
+      </div>
+    </HomeSection>
+  );
+};
 interface HomeSectionProps {
   containerClassName?: string;
   innerContainerClassName?: string;
