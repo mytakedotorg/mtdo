4c822f0586196c7b55159581d818769aa7b5ef50
Ned Twigg <ned.twigg@diffplug.com>
2020-09-26 14:45:06 -0700

    Refactor FactApiTest to allow testing multiple implementations.

--- server/src/test/java/controllers/FactApiTest.java
+++ server/src/test/java/controllers/FactApiTest.java
@@ -19,35 +19,38 @@
  */
 package controllers;
 
-import common.JoobyDevRule;
 import io.restassured.RestAssured;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.jooby.Jooby;
 import org.junit.Assert;
-import org.junit.ClassRule;
 import org.junit.Test;
 
 public class FactApiTest {
-	static class App extends Jooby {
-		{
-			use(new FactApi());
+	@Test
+	public void prod() throws Exception {
+		testFactModule(new FactApi());
+	}
+
+	private void testFactModule(Jooby.Module factApiModule) throws Exception {
+		try (AutoCloseable server = startServer(factApiModule)) {
+			byte[] actualBytes = RestAssured.given().get("/api/fact/1/c2a9d475e448866c48a1f7c0d5f98c6fc2bfe6a3").then()
+					.contentType("application/json")
+					.extract().body().asByteArray();
+			String actual = new String(actualBytes, StandardCharsets.UTF_8);
+			byte[] expectedBytes = Files.readAllBytes(Paths.get("../factset-tooling/src/test/resources/org/mytake/factset/gradle/kennedy-nixon-1-of-4.json"));
+			String expected = new String(expectedBytes, StandardCharsets.UTF_8);
+			Assert.assertEquals(FactApi.recondense(expected), actual);
 		}
 	}
 
-	@ClassRule
-	public static JoobyDevRule app = new JoobyDevRule(new App());
-
-	@Test
-	public void factTest() throws IOException {
-		byte[] actualBytes = RestAssured.given().get("/api/fact/1/c2a9d475e448866c48a1f7c0d5f98c6fc2bfe6a3").then()
-				.contentType("application/json")
-				.extract().body().asByteArray();
-		String actual = new String(actualBytes, StandardCharsets.UTF_8);
-		byte[] expectedBytes = Files.readAllBytes(Paths.get("../factset-tooling/src/test/resources/org/mytake/factset/gradle/kennedy-nixon-1-of-4.json"));
-		String expected = new String(expectedBytes, StandardCharsets.UTF_8);
-		Assert.assertEquals(FactApi.recondense(expected), actual);
+	private AutoCloseable startServer(Jooby.Module factApiModule) {
+		Jooby jooby = new Jooby();
+		jooby.use(factApiModule);
+		jooby.start("server.join=false");
+		return () -> {
+			jooby.stop();
+		};
 	}
 }
