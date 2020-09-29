4f45d16f57f41a4cbd0b5b9b7a0e2fbe48a117fa
Ned Twigg <ned.twigg@diffplug.com>
2020-09-29 16:14:13 -0700

    Fix broken test.

--- server/src/test/java/auth/LoginLogoutTest.java
+++ server/src/test/java/auth/LoginLogoutTest.java
@@ -52,7 +52,7 @@ public class LoginLogoutTest {
 
 		EmailAssert loginEmail = dev.waitForEmail();
 		loginEmail.subject().isEqualTo("Welcome to MyTake.org");
-		loginEmail.body().contains("Welcome back to MyTake.org, samples@email.com!");
+		loginEmail.body().contains("Welcome to MyTake.org, samples@email.com!");
 		String link = loginEmail.extractLink("Visit ");
 
 		String loginCookie = AuthModuleHarness.authTokenValue(dev, "samples");
