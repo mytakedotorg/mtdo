ca20bd78dd08ee3e8694320dd132c63a12476ab7
Ned Twigg <ned.twigg@diffplug.com>
2020-10-07 15:18:20 -0700

    Fix unused imports.

--- factset-tooling/src/main/java/org/mytake/factset/gradle/GuiTask.java
+++ factset-tooling/src/main/java/org/mytake/factset/gradle/GuiTask.java
@@ -37,17 +37,11 @@ import com.diffplug.gradle.eclipse.MavenCentralPlugin;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.function.Consumer;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.file.FileCollection;
-import org.gradle.api.tasks.JavaExec;
 
 class GuiTask {
 	private static final String GUI_CONFIG = "gui";
@@ -96,19 +90,19 @@ class GuiTask {
 		});
 	}
 
-//	static Set<File> fromLocalClassloader() {
-//		Set<File> files = new LinkedHashSet<>();
-//		Consumer<Class<?>> addPeerClasses = clazz -> {
-//			URLClassLoader urlClassloader = (URLClassLoader) clazz.getClassLoader();
-//			for (URL url : urlClassloader.getURLs()) {
-//				String name = url.getFile();
-//				if (name != null) {
-//					files.add(new File(name));
-//				}
-//			}
-//		};
-//		// add the gradle API
-//		addPeerClasses.accept(JavaExec.class);
-//		return files;
-//	}
+	//	static Set<File> fromLocalClassloader() {
+	//		Set<File> files = new LinkedHashSet<>();
+	//		Consumer<Class<?>> addPeerClasses = clazz -> {
+	//			URLClassLoader urlClassloader = (URLClassLoader) clazz.getClassLoader();
+	//			for (URL url : urlClassloader.getURLs()) {
+	//				String name = url.getFile();
+	//				if (name != null) {
+	//					files.add(new File(name));
+	//				}
+	//			}
+	//		};
+	//		// add the gradle API
+	//		addPeerClasses.accept(JavaExec.class);
+	//		return files;
+	//	}
 }
