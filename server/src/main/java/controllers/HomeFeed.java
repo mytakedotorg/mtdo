bfec11bee8eb54732505599860f983da950fae7e
Ned Twigg <ned.twigg@diffplug.com>
2020-11-01 23:12:13 -0800

    Give homepage a slightly more customized version of social metadata.

--- server/src/main/java/controllers/HomeFeed.java
+++ server/src/main/java/controllers/HomeFeed.java
@@ -32,7 +32,7 @@ public class HomeFeed implements Jooby.Module {
 	@Override
 	public void configure(Env env, Config conf, Binder binder) throws Throwable {
 		env.router().get(URL, (req, res) -> {
-			CacheControl.hour(res).send(views.Placeholder.home.template(SocialEmbed.search("election,rigged")));
+			CacheControl.hour(res).send(views.Placeholder.home.template(SocialEmbed.homepage()));
 		});
 	}
 }
