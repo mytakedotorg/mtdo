a0aaec6e4a2d49f992e64bf36336f649068a2aad
Ned Twigg <ned.twigg@diffplug.com>
2021-04-22 12:06:42 -0700

    Adapt JDK 11 reflection.

--- buildSrc/src/main/java/org/mytake/gradle/jsweet/JSweetTranspileTask.java
+++ buildSrc/src/main/java/org/mytake/gradle/jsweet/JSweetTranspileTask.java
@@ -91,11 +91,11 @@ public class JSweetTranspileTask extends DefaultTask {
 			if (configuration.getFactoryClassName() != null) {
 				try {
 					factory = (JSweetFactory) Thread.currentThread().getContextClassLoader()
-							.loadClass(configuration.getFactoryClassName()).newInstance();
+							.loadClass(configuration.getFactoryClassName()).getDeclaredConstructor().newInstance();
 				} catch (Exception e) {
 					try {
 						// try forName just in case
-						factory = (JSweetFactory) Class.forName(configuration.getFactoryClassName()).newInstance();
+						factory = (JSweetFactory) Class.forName(configuration.getFactoryClassName()).getDeclaredConstructor().newInstance();
 					} catch (Exception e2) {
 						throw new Exception("cannot find or instantiate factory class: "
 								+ configuration.getFactoryClassName()
