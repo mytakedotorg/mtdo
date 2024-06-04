a4e72775b4ccd29c0cac9720c69f176385f9ebb5
Ned Twigg <ned.twigg@diffplug.com>
2024-06-04 14:45:01 -0700

    Adapt `CleanPostgresModule` for GitHub instead of CircleCI.

--- server/src/test/java/common/GitHubCI.java
+++ server/src/test/java/common/GitHubCI.java
@@ -1,6 +1,6 @@
 /*
  * MyTake.org website and tooling.
- * Copyright (C) 2017-2020 MyTake.org, Inc.
+ * Copyright (C) 2017-2024 MyTake.org, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -19,8 +19,8 @@
  */
 package common;
 
-public class CircleCi {
-	public static boolean isCircle() {
-		return System.getenv().containsKey("CIRCLECI");
+public class GitHubCI {
+	public static boolean isGitHubCI() {
+		return System.getenv().containsKey("GITHUB_ACTION");
 	}
 }
